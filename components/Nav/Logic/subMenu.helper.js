f9ca5895b1816fe203fef6af8aecfe99171807e8
Rajeev <rranjan.ra15@gmail.com>
2022-04-11 15:48:26 +0530

    added left drop-down menu and submenu

---
+++ components/Nav/Logic/subMenu.helper.js
@@ -0,0 +1,45 @@
+export const languages = [
+    {
+        title:'Hindi',
+        link:'/'
+    },
+    {
+        title:'Marathi',
+        link:'/'
+    },
+    {
+        title:'Punjabi',
+        link:'/'
+    },
+    {
+        title:'Haryanavi',
+        link:'/'
+    },
+    {
+        title:'Maithali',
+        link:'/'
+    },
+    {
+        title:'Russian',
+        link:'/'
+    },
+]
+
+export const preferences = [
+    {
+        title:'Preference 1',
+        link:'/'
+    },
+    {
+        title:'Preference 2',
+        link:'/'
+    },
+    {
+        title:'Preference 3',
+        link:'/'
+    },
+    {
+        title:'Preference 4',
+        link:'/'
+    },
+]
