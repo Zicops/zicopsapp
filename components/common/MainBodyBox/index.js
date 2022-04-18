789e11524a7232757e27897525521a0e5ab58b91
shawnkit70 <71093268+shawnkit70@users.noreply.github.com>
2022-04-18 19:22:18 +0530

    mainBodyBox

--- components/common/MainBodyBox/index.js
+++ components/common/MainBodyBox/index.js
@@ -1,6 +1,4 @@
 import styles from './mainBodyBox.module.scss';
-import RadioButton from '../RadionButton';
-import ExamFooter from '../ExamFooter';
 
 const MainBodyBox = ({ children }) => {
   return <div className={`${styles.content_panel}`}>{children}</div>;
