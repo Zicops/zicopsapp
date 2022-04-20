54b0a6c2f4d1f5adf6ba4915e7bb8e293fd4b5af
Rajeev <rranjan.ra15@gmail.com>
2022-04-20 11:38:12 +0530

    addeed common calendar and its styles(styles are in global)

---
+++ components/common/CommonCalendar/index.js
@@ -0,0 +1,28 @@
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import Clock from 'react-digital-clock';
+import styles from './calendar.module.scss';
+
+const CommonCalendar = () => {
+  let today = new Date();
+  let date = new Date().toUTCString().slice(5, 16);
+  return (
+    <div>
+      <DatePicker
+        dateFormat="dd/MM/yyyy"
+        className="common_calendar_body"
+        calendarClassName="common_calendar"
+        dayClassName={() => 'common_calendar_dates'}
+        monthClassName={() => 'common_calendar_months'}
+        inline
+        fixedHeight
+      />
+      <div className={`${styles.clock_time}`}>
+        <span>{date}</span>
+        <Clock format={'hh-mm'} />
+      </div>
+    </div>
+  );
+};
+
+export default CommonCalendar;
