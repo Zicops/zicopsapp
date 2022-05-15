da20578791c6d86ead46e0b223ad1f42db0581de
Jayanta Sarkar <jtas.sarkar@gmail.com>
2022-05-15 22:56:27 +0530

    crossorigin="use-credentials"

--- components/CustomVideoPlayer/VideoPlayer/index.js
+++ components/CustomVideoPlayer/VideoPlayer/index.js
@@ -57,6 +57,7 @@ export default function VideoPlayer({
       {videoData.type === 'mp4' && videoData.videoSrc && (
         <>
           <video
+            crossorigin="use-credentials"
             tabIndex="0"
             onClick={handleClick}
             onKeyDown={handleKeyDown}
