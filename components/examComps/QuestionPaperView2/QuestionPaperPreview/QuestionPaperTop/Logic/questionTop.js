d1248b0c5a36c1ca012e46a528903b067c70575c
shawnkit70 <71093268+shawnkit70@users.noreply.github.com>
2022-05-25 02:53:13 +0530

    added homepage component

--- components/examComps/QuestionPaperView2/QuestionPaperPreview/QuestionPaperTop/Logic/questionTop.js
+++ components/examComps/QuestionPaperView2/QuestionPaperPreview/QuestionPaperTop/Logic/questionTop.js
@@ -6,5 +6,7 @@ export const obj = {
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
   Total_Q: '30',
   Total_M: '60',
-  SectionWise: 'Yes'
+  SectionWise: 'Yes',
+  Passing_M: '20',
+  Duration: '60 mins'
 };
