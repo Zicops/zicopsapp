d6386a90a317cd9b835b2256785a6b38a2f363eb
Jayanta Sarkar <jtas.sarkar@gmail.com>
2022-05-25 19:14:20 +0530

    notes added in player

--- components/Nav/Logic/useDropDownHandle.js
+++ components/Nav/Logic/useDropDownHandle.js
@@ -2,8 +2,6 @@ import { useState, useEffect, useContext } from "react";
 
 import { userContext } from '../../../state/contexts/UserContext';
 
-
-
 export function useDropDownHandle (){
     const { isAdmin, makeAdmin } = useContext(userContext);
 
@@ -40,9 +38,17 @@ export function useDropDownHandle (){
       setAnchorEl(event.currentTarget);
     };
   
-    function handleClose  ()  {
-      setAnchorEl(null);
-    };  
+  function handleClose(e) {
+    // prevent closing if the pointer is over menu list
+    if (e?.relatedTarget?.classList) {
+      const classList = [...e?.relatedTarget?.classList];
+      console.log(classList);
+
+      if (classList.includes('MuiMenuItem-root')) return;
+    }
+
+    setAnchorEl(null);
+  };  
 
     return {isAdmin,gotoAdmin,gotoUser,open,anchorEl,handleClick,handleClose};
 }
