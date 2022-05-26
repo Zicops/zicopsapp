776280b9b91c38a79c7011fb6dd33477f9e523be
shawnkit70 <71093268+shawnkit70@users.noreply.github.com>
2022-05-26 15:22:01 +0530

    changes in homepage

--- components/HomePage/index.js
+++ components/HomePage/index.js
@@ -1,50 +1,16 @@
+import HomePages from './HomePages';
 import styles from './home.module.scss';
-import ScrollItems from './ScrollItems';
 import { data } from './Logic/homePage.helper';
-import HomeInputField from './HomeInputField';
-import { useEffect } from 'react';
 
 const HomePage = () => {
-  function handleScroll() {
-    console.log(window.scrollY);
-  }
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-  }, []);
-
   return (
-    <div className={`${styles.HomeContainer}`}>
-      <header className={`${styles.HomeHeader}`}>
-        <div className={`${styles.ZicopsLogo}`}>
-          <img src="./images/zicops-header-logo.png" alt="not found" />
+    <>
+      {data.map((item) => (
+        <div className={`${styles.HomeMainContainer}`}>
+          <HomePages item={item} />
         </div>
-        <div className={`${styles.Login}`}>
-          <img src="./images/Union1.png" alt="not found" />
-          <a href="/home">Log In</a>
-        </div>
-      </header>
-      <div className={`${styles.HomeBody}`}>
-        <div className={`${styles.scrollContainer} ${styles.y}`}>
-          {/* {data.map((item) => ( */}
-          <ScrollItems item={data[0]} />
-          {/* ))} */}
-        </div>
-        <div className={`${styles.btn}`}>
-          <button>See More</button>
-        </div>
-        <HomeInputField />
-      </div>
-      <footer className={`${styles.HomeFooter}`}>
-        <div className={`${styles.HomeFooterInner}`}>
-          <a href="/home">Zicops About</a>
-          <a href="/home">Zicops About</a>
-          <a href="/home">Zicops About</a>
-          <a href="/home">Zicops About</a>
-          <a href="/home">Zicops About</a>
-        </div>
-      </footer>
-    </div>
+      ))}
+    </>
   );
 };
 
