c9e4b515dc53d0a317b448ab61960c44a25df7c6
VAJRESH <vajresh005@gmail.com>
2022-05-31 11:19:51 +0530

    added infinte scroll in search body

--- components/SearchBody/index.js
+++ components/SearchBody/index.js
@@ -1,39 +1,84 @@
+import { useLazyQuery } from '@apollo/client';
+import { useEffect, useRef, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { GET_LATEST_COURSES, queryClient } from '../../../learning/API/Queries';
+import { ToastMsgAtom } from '../../state/atoms/toast.atom';
 import Card from '../common/Card';
-import styles from './searchBody.module.scss'
+import styles from './searchBody.module.scss';
+
 const SearchBody = () => {
-    const data = [
-      { id: 0, img: '/images/courses/workplace design.png' },
-      { id: 1, img: '/images/courses/website_developer.png' },
-      { id: 2, img: '/images/courses/scripting with python.png' },
-      { id: 3, img: '/images/courses/sre and devops.png' },
-      { id: 4, img: '/images/courses/47.png' },
-      { id: 5, img: '/images/courses/46.png' },
-      { id: 6, img: '/images/courses/14.png' },
-      { id: 7, img: '/images/courses/49.png' },
-      { id: 8, img: '/images/courses/45.png' },
-      { id: 9, img: '/images/courses/6.png' },
-      { id: 10, img: '/images/courses/5.png' },
-      { id: 11, img: '/images/courses/4.png' },
-      { id: 12, img: '/images/courses/3.png' },
-      { id: 13, img: '/images/courses/2.png' },
-      { id: 14, img: '/images/courses/1.png' },
-      { id: 15, img: '/images/courses/4.png' },
-      { id: 16, img: '/images/courses/3.png' },
-      { id: 17, img: '/images/courses/2.png' }
-      // { id: 18, img: '/images/courses/1.png' }
-    ];
-    return (
-        <>
-        <div className={`${styles.searchBodyTitle}`}>
-        Search Results
-        </div>
-        <div className={`${styles.searchBody}`}>
-          {data.map((dt) => (
-            <Card key={dt.id} data={ dt }/>
-          ))}
-        </div>
-      </>
-    );
-}
- 
+  const [loadCourses, { error: loadCoursesError, refetch: refetchCourses }] = useLazyQuery(
+    GET_LATEST_COURSES,
+    { client: queryClient }
+  );
+
+  const [toastMsg, setToastMsg] = useRecoilState(ToastMsgAtom);
+
+  const lastItemRef = useRef(null);
+
+  // state for storing courses data
+  const [courses, setCourses] = useState([]);
+  const [pageCursor, setPageCursor] = useState(null);
+  const [refetchData, setRefetchData] = useState(null);
+
+  // load table data
+  useEffect(() => {
+    const queryVariables = { publish_time: Date.now(), pageSize: 10, pageCursor: '' };
+
+    loadCourses({ variables: queryVariables }).then(({ data }) => {
+      if (loadCoursesError) return setToastMsg({ type: 'danger', message: 'course load error' });
+
+      const courseData = data?.latestCourses;
+      setPageCursor(courseData?.pageCursor || null);
+      setCourses(courseData?.courses || []);
+    });
+  }, []);
+
+  function refetchNextCourses() {
+    if (!pageCursor) return;
+    refetchCourses({ pageCursor: pageCursor }).then(({ data: { latestCourses } }) => {
+      console.log('refecth', 'course length', courses.length + latestCourses?.courses?.length);
+      setPageCursor(latestCourses?.pageCursor || null);
+      setCourses([...courses, ...latestCourses?.courses]);
+    });
+
+    if (loadCoursesError) return setToastMsg({ type: 'danger', message: 'course load error' });
+  }
+
+  // for reloading new courses
+  useEffect(() => {
+    var options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1.0
+    };
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        setRefetchData(true);
+      }
+    }, options);
+    if (lastItemRef?.current) observer.observe(lastItemRef?.current);
+  }, []);
+
+  useEffect(() => {
+    if (refetchData) {
+      refetchNextCourses();
+      setRefetchData(false);
+    }
+  }, [refetchData]);
+
+  return (
+    <>
+      <div className={`${styles.searchBodyTitle}`}>Search Results</div>
+      <div className={`${styles.searchBody}`}>
+        {courses.map((course) => (
+          <Card data={course} key={course.id} />
+        ))}
+      </div>
+
+      <span ref={lastItemRef}></span>
+    </>
+  );
+};
+
 export default SearchBody;
