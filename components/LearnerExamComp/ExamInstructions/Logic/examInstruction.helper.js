aedf30d794bb65e3c70e58813280e1799b2d2fe8
shawnkit70 <71093268+shawnkit70@users.noreply.github.com>
2022-06-01 19:25:37 +0530

    Exam instruction page

---
+++ components/LearnerExamComp/ExamInstructions/Logic/examInstruction.helper.js
@@ -0,0 +1,16 @@
+export const data = {
+  title: 'Core Java Fundamentals',
+  examType: 'Scheduled',
+  examDuration: '3 hrs',
+  examStartTime: '7 Am',
+  examNQuestions: '50',
+  examPassingCriteria: '60%',
+  examMaxAttempt: '3 Attempts',
+  examDate: '25/05/2022',
+  examTimeStandard: 'IST',
+  examBufferTime: '30 mins',
+  examEndTime: '10:30 Am',
+  examTotalMarks: '100',
+  examProctored: 'No',
+  examAttempt: '1/3'
+};
