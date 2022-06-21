0a716ac86917edf94b9e865dc20e8bf90e4dab63
Roshan Kumar Chatei <roshankumarchatei@Roshans-MacBook-Air.local>
2022-06-21 08:44:23 +0530

    changed login page to initial value

--- pages/login.js
+++ pages/login.js
@@ -7,7 +7,7 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 
 const Login = () => {
-  const [page, setPage] = useState(3);
+  const [page, setPage] = useState(0);
 
   return (
     <>
