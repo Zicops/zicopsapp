2ce0786030e2211d8ba8661ff8605fc2d4a8dfc4
shawnkit70 <71093268+shawnkit70@users.noreply.github.com>
2022-06-28 04:11:32 +0530

    changes in fireBaseConfig

--- helper/firebaseUtil/firebaseConfig.js
+++ helper/firebaseUtil/firebaseConfig.js
@@ -2,12 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD05Uj8S-YumeJUiM4xuO8YFP7rjLJbrP8',
-  authDomain: 'http://zicops-one.firebaseapp.com/',
-  projectId: 'zicops-one',
-  storageBucket: 'http://zicops-one.appspot.com/',
-  messagingSenderId: '359144709511',
-  appId: '1:359144709511:web:d8d41fb236983ea45f4722'
+  apiKey: 'AIzaSyCiaxV70znBE3Q7hpPbQMitK1QVrNLFGiQ',
+  authDomain: 'zicops-app.firebaseapp.com',
+  projectId: 'zicops-app',
+  storageBucket: 'zicops-app.appspot.com',
+  messagingSenderId: '414174583675',
+  appId: '1:414174583675:web:f326a3f335f9fbf672fbde',
+  measurementId: 'G-3TBV52MMGC'
 };
 
 export const app = initializeApp(firebaseConfig);
