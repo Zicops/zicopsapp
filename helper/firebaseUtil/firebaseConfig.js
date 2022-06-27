2ae29e621b51a187ce2c3e56a5075e8ed315df45
shawnkit70 <71093268+shawnkit70@users.noreply.github.com>
2022-06-28 03:54:07 +0530

    apiKey in fireBaseConfig

--- helper/firebaseUtil/firebaseConfig.js
+++ helper/firebaseUtil/firebaseConfig.js
@@ -2,13 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: 'AIzaSyD05Uj8S-YumeJUiM4xuO8YFP7rjLJbrP8',
+  authDomain: 'http://zicops-one.firebaseapp.com/',
+  projectId: 'zicops-one',
+  storageBucket: 'http://zicops-one.appspot.com/',
+  messagingSenderId: '359144709511',
+  appId: '1:359144709511:web:d8d41fb236983ea45f4722'
 };
 
 export const app = initializeApp(firebaseConfig);
