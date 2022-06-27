cec63d879ff621ebc62b26c59abbb860e2a95117
VAJRESH <vajresh005@gmail.com>
2022-06-27 13:10:38 +0530

    question paper name is displayed
    https://github.com/Zicops/zicopsapp/issues/217

--- pages/admin/exams/my-question-papers/add/[questionPaperId].js
+++ pages/admin/exams/my-question-papers/add/[questionPaperId].js
@@ -1,3 +1,5 @@
+import { QuestionPaperTabDataAtom } from '@/state/atoms/exams.atoms';
+import { useRecoilValue } from 'recoil';
 import QuestionPaperTab from '../../../../../components/AdminExamComps/QuestionPapers/QuestionPaperTab';
 import AdminHeader from '../../../../../components/common/AdminHeader';
 import MainBody from '../../../../../components/common/MainBody';
@@ -6,11 +8,13 @@ import Sidebar from '../../../../../components/common/Sidebar';
 import { examSidebarData } from '../../../../../components/common/Sidebar/Logic/sidebar.helper';
 
 export default function AddQuestionPaper() {
+  const questionPaperTabData = useRecoilValue(QuestionPaperTabDataAtom);
+
   return (
     <>
       <Sidebar sidebarItemsArr={examSidebarData} />
       <MainBody>
-        <AdminHeader title="Edit Question Papers" />
+        <AdminHeader title={questionPaperTabData?.paperMaster?.name || 'Edit Question Papers'} />
 
         <MainBodyBox>
           <QuestionPaperTab />
