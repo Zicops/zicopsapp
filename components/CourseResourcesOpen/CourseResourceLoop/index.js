496abe44492ba3e7703942ae7a133834525d9d77
shawnkit70 <71093268+shawnkit70@users.noreply.github.com>
2022-07-15 01:39:01 +0530

    images for resoiurce

--- components/CourseResourcesOpen/CourseResourceLoop/index.js
+++ components/CourseResourcesOpen/CourseResourceLoop/index.js
@@ -5,12 +5,33 @@ import styles from '../courseResourcesOpen.module.scss';
 
 const CourseResourceLoop = ({ resource }) => {
   const [showDoc, setShowDoc] = useState(false);
+  let resourceImage;
+  switch (resource.type) {
+    case 'LINK':
+      resourceImage = '/images/URL-icon.png';
+      break;
+    case 'PDF':
+      resourceImage = '/images/PDF-icon.png';
+      break;
+    case 'DOC':
+      resourceImage = '/images/DOC-icon.png';
+      break;
+    case 'DOCX':
+      resourceImage = '/images/DOCX-icon.png';
+      break;
+    case 'PPT':
+      resourceImage = '/images/PPT-icon.png';
+      break;
+    case 'EXCEL':
+      resourceImage = '/images/media-container.png';
+      break;
+  }
 
   return (
     <>
       <div className={`${styles.resource_loop}`}>
         <div className={`${styles.image_box}`}>
-          <img src="/images/pdfIcon.png" alt="" />
+          <img src={`${resourceImage}`} alt="" />
         </div>
         <div className={`${styles.data}`}>
           <div className={`${styles.data_title}`}>{resource.name}</div>
