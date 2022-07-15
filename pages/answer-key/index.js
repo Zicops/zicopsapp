ae5ae7c4c44adab3dd93f08a2131450a773f1fb6
VAJRESH <vajresh005@gmail.com>
2022-07-15 09:30:38 +0530

    added answer key page and related styles

---
+++ pages/answer-key/index.js
@@ -0,0 +1,52 @@
+import AttemptsTable from '@/components/common/AttemptsTable';
+import LabeledDropdown from '@/components/common/FormComponents/LabeledDropdown';
+import { getResultStyles } from '@/components/Congratulations/Logic/congratulationsHead.helper';
+import { LearnerExamAtom } from '@/state/atoms/exams.atoms';
+import { useRouter } from 'next/router';
+import { useRecoilValue } from 'recoil';
+import styles from './answerKey.module.scss';
+
+export default function AnswerKeyPage() {
+  const learnerExamData = useRecoilValue(LearnerExamAtom);
+  const router = useRouter();
+
+  return (
+    <div className={`${styles.answerKey}`}>
+      <div className={`${styles.backBtn}`}>
+        <button onClick={() => router.back()}>
+          <img src="./images/Back.png" />
+        </button>
+      </div>
+
+      <h2 className={`h2`}>{learnerExamData?.examData?.name || 'Exam Name'}</h2>
+
+      <LabeledDropdown
+        dropdownOptions={{
+          placeholder: 'Attempts',
+          options: [],
+          // value: { value: examTabData?.no_attempts, label: examTabData?.no_attempts },
+          isDisabled: true
+        }}
+        styleClass={styles.dropdownInput}
+        isFiftyFifty={true}
+        changeHandler={(e) => changeHandler(e, examTabData, setExamTabData, 'no_attempts')}
+      />
+
+      <div className={`${styles.tableContainer}`}>
+        <p>Results</p>
+        <AttemptsTable
+          totalAttempts={3}
+          attemptData={[
+            {
+              attempt: 1,
+              examScore: learnerExamData?.resultData?.examScore,
+              totalMarks: learnerExamData?.examData?.totalMarks,
+              result: learnerExamData?.resultData?.isPassed ? 'PASS' : 'FAIL'
+            }
+          ]}
+          style={getResultStyles(learnerExamData?.resultData?.isPassed ? 'PASS' : 'FAIL')}
+        />
+      </div>
+    </div>
+  );
+}
