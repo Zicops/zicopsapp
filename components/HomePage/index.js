36205b62007127360832cf2689765c1e117bebcb
VAJRESH <vajresh005@gmail.com>
2022-07-16 17:20:09 +0530

    home animation updated

--- components/HomePage/index.js
+++ components/HomePage/index.js
@@ -1,34 +1,53 @@
-import { useState, useEffect } from 'react';
-import {
-  CSSTransition,
-  SwitchTransition,
-  Transition,
-  TransitionGroup
-} from 'react-transition-group';
-import HomePages from './HomePages';
+import { useEffect, useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import styles from './home.module.scss';
-import { data } from './Logic/homePage.helper';
-import Link from 'next/link';
-import HomeInputField from './HomeInputField';
 import HomeHeader from './HomeHeader';
+import HomeInputField from './HomeInputField';
+import HomePages from './HomePages';
+import { data } from './Logic/homePage.helper';
 
-const HomePage = () => {
-  const [scrollDown, setScrollDown] = useState(0);
+export default function HomePage() {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const [slideData, setSlideData] = useState({
+    activeSlide: 0,
+    isLastScrollUp: false
+  });
+  const [deltaY, setDeltaY] = useState(null);
   const [slide, setSlide] = useState(false);
 
-  function handleScrollAndZoom(e) {
-    setSlide(!slide);
-    if (e.deltaY < 0 && scrollDown > 0) {
-      setScrollDown(--scrollDown);
-    } else {
-      setScrollDown(scrollDown);
-    }
-    if (e.deltaY > 0 && scrollDown < 3) {
-      setScrollDown(++scrollDown);
-    } else {
-      setScrollDown(scrollDown);
-    }
-  }
+  const maxSlideCount = 3;
+  let wait = false;
+  useEffect(() => {
+    if (wait) return;
+
+    wait = true;
+    const handler = setTimeout(() => {
+      wait = false;
+      const isScrollUp = deltaY > 0;
+      const index = slideIndex + (isScrollUp ? 1 : -1);
+
+      if (index < 0) index = 0;
+      if (index > maxSlideCount) index = maxSlideCount;
+
+      if (index !== slideIndex) {
+        setSlide(!slide);
+
+        setSlideData({
+          ...slideData,
+          activeSlide: index,
+          isLastScrollUp: isScrollUp
+        });
+      }
+
+      setSlideIndex(index);
+    }, 100);
+
+    return () => clearTimeout(handler);
+  }, [deltaY]);
+
+  useEffect(() => {
+    console.log(slideIndex);
+  }, [slideIndex]);
 
   function showImage() {
     alert('enter');
@@ -39,40 +58,101 @@ const HomePage = () => {
   // useEffect(() => {
   //   setSlide(() => scrollDown);
   // }, [scrollDown]);
+
   return (
     <div className={`${styles.container}`}>
       <HomeHeader />
-      <CSSTransition
-        in={slide}
+
+      {[0, 1, 2, 3].map((i) => {
+        const { activeSlide, isLastScrollUp } = slideData;
+        let count = isLastScrollUp ? 1 : -1;
+        let nextSlide = activeSlide + count;
+
+        if (nextSlide < 0 || nextSlide > maxSlideCount) nextSlide = null;
+
+        return (
+          <CSSTransition
+            in={activeSlide === i}
+            timeout={2000}
+            classNames={{
+              enterActive: nextSlide === i ? styles.zoomOut : styles.zoomIn,
+              exitActive: nextSlide === i ? styles.zoomIn : styles.zoomOut
+            }}>
+            <div
+              className={`${styles.scrollItems}  ${activeSlide === i ? '' : styles.scaleDown}`}
+              onWheel={(e) => setDeltaY(e.deltaY)}>
+              <HomePages item={data[i]} />
+            </div>
+          </CSSTransition>
+        );
+      })}
+
+      {/* <CSSTransition
+        in={slideData?.activeSlide === 0}
         timeout={2000}
         classNames={{
-          appear: styles.zoomOut,
-          appearActive: styles.zoomOut,
-          appearDone: styles.zoomIn,
-          // enter: 'my-enter',
-          // enterActive: 'my-active-enter',
-          // enterDone: 'my-done-enter',
-          // exit: 'my-exit',
-          // exitActive: 'my-active-exit',
-          // exitDone: 'my-done-exit',
-          enterActive: styles.zoomIn,
-          exitActive: styles.zoomOut
-        }}
-        // onEnter={showImage}
-        // onEntering = {showImage}
-        // onEntered={removeOpacity}
-        // onExit
-        // onExiting
-        // onExited={hideImage}
-        // className={`${styles.mynode}`}
-      >
-        <div className={`${styles.scrollItems}`} onWheel={handleScrollAndZoom}>
-          <HomePages item={data[scrollDown]} />
+          enterActive: !slideData.isLastScrollUp&&slideData?.activeSlide  ? styles.zoomIn : styles.zoomOut,
+          exitActive: slideData.isLastScrollUp ? styles.zoomIn : styles.zoomOut
+        }}>
+        <div
+          className={`${styles.scrollItems}  ${
+            slideData?.activeSlide === 0 ? '' : styles.scaleDown
+          }`}
+          onWheel={(e) => setDeltaY(e.deltaY)}>
+          <HomePages item={data[0]} />
         </div>
       </CSSTransition>
+
+      <CSSTransition
+        in={slideData?.activeSlide === 1}
+        timeout={2000}
+        classNames={{
+          enterActive: !slideData.isLastScrollUp ? styles.zoomIn : styles.zoomOut,
+          exitActive: slideData.isLastScrollUp ? styles.zoomIn : styles.zoomOut
+        }}>
+        <div
+          className={`${styles.scrollItems}  ${
+            slideData?.activeSlide === 1 ? '' : styles.scaleDown
+          }`}
+          onWheel={(e) => setDeltaY(e.deltaY)}>
+          <HomePages item={data[1]} />
+        </div>
+      </CSSTransition> */}
+
       <HomeInputField />
     </div>
   );
-};
+}
 
-export default HomePage;
+// {
+//   /* <CSSTransition
+//   in={slide}
+//   timeout={2000}
+//   // className="fade"
+//   classNames={{
+//     // appear: styles.zoomOut,
+//     // appearActive: styles.zoomOut,
+//     // appearDone: styles.zoomIn,
+//     // enter: 'my-enter',
+//     // enterActive: 'my-active-enter',
+//     // enterDone: 'my-done-enter',
+//     // exit: 'my-exit',
+//     // exitActive: 'my-active-exit',
+//     // exitDone: styles.zoomOut,
+//     // enterActive: styles.zoomIn,
+//     enterActive: deltaY < 0 ? styles.zoomIn : styles.zoomOut,
+//     exitActive: deltaY < 0 ? styles.zoomIn : styles.zoomOut
+//   }}
+//   // onEnter={() => console.log('enter')}
+//   // onEntering = {showImage}
+//   // onEntered={removeOpacity}
+//   // onExit
+//   // onExiting
+//   // onExited={hideImage}
+//   // className={`${styles.mynode}`}
+// >
+//   <div className={`${styles.scrollItems}`} onWheel={(e) => setDeltaY(e.deltaY)}>
+//     <HomePages item={data[slideIndex]} />
+//   </div>
+// </CSSTransition> */
+// }
