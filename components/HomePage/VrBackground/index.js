5015605193292dca530c2697493906a288dd0222
VAJRESH <vajresh005@gmail.com>
2022-07-18 00:44:39 +0530

    vr background image added in home page

---
+++ components/HomePage/VrBackground/index.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import styles from '../home.module.scss';
+
+export default function VrBackground() {
+  const [duration, setDuration] = useState(150000);
+
+  useEffect(() => {
+    let timer = null;
+    function handleMouseMove(e) {
+      console.log(e);
+      clearTimeout(timer);
+
+      timer = setTimeout(() => {
+        console.log(e.screenX * 100);
+        setDuration(e.screenX * 100);
+      }, 100);
+    }
+
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  }, []);
+
+  return (
+    <>
+      {/* https://codepen.io/ssudoku/pen/prxEjg */}
+      <section className={`${styles.vrContainer}`}>
+        <a-scene className={`${styles.vrBackground}`}>
+          <a-entity rotation="0 0 0">
+            <a-animation
+              attribute="rotation"
+              dur={duration}
+              easing="linear"
+              fill="forwards"
+              to="0 360 0"
+              repeat="indefinite"></a-animation>
+            {/* <a-sky src="https://upload.wikimedia.org/wikipedia/commons/1/18/Rheingauer_Dom%2C_Geisenheim%2C_360_Panorama_%28Equirectangular_projection%29.jpg"></a-sky> */}
+            {/* <a-sky src="/images/galaxy.jpg"></a-sky> */}
+            <a-sky src="/images/starrySky2.png"></a-sky>
+          </a-entity>
+        </a-scene>
+      </section>
+    </>
+  );
+}
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,7 +1,30 @@
+import VrBackground from '@/components/HomePage/VrBackground';
+import Head from 'next/head';
+import { useEffect, useRef, useState } from 'react';
+import { createPortal } from 'react-dom';
 import HomePage from '../../components/HomePage';
 
 const Home = () => {
-  return <HomePage />;
+  const ref = useRef();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    ref.current = document.body;
+
+    setMounted(true);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <script src="https://aframe.io/releases/0.5.0/aframe.min.js"></script>
+      </Head>
+
+      {mounted ? createPortal(<VrBackground />, ref.current) : null}
+
+      <HomePage />
+    </>
+  );
 };
 
 export default Home;
