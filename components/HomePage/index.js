361d761d9df39faff508312377063faa5b8794f1
VAJRESH <vajresh005@gmail.com>
2022-07-19 01:01:25 +0530

    home updated
    home page logo added login
    ingress self added

--- components/HomePage/index.js
+++ components/HomePage/index.js
@@ -5,6 +5,7 @@ import HomeHeader from './HomeHeader';
 import HomeInputField from './HomeInputField';
 import SingleSlide from './SingleSlide';
 import { data } from './Logic/homePage.helper';
+import SlideIndicator from './SlideIndicator';
 
 export default function HomePage() {
   const [slideIndex, setSlideIndex] = useState(0);
@@ -15,34 +16,43 @@ export default function HomePage() {
   });
 
   const DURATION = 1000;
-  const maxSlideCount = 3;
+  const maxSlideCount = data.length || 0;
 
   function showSlidesOnScroll(e) {
     if (isAnimationOngoing) return;
     seIsAnimationOngoing(1);
-      const isScrollUp = e.deltaY > 0;
-      let index = slideIndex + (isScrollUp ? 1 : -1);
-      if (index < 0) index = 0;
-      if (index > maxSlideCount) index = maxSlideCount;
-      if (index !== slideIndex) {
-        setSlideData({
-          ...slideData,
-          activeSlide: index,
-          isLastScrollUp: isScrollUp
-        });
-      }
+    const isScrollUp = e.deltaY > 0;
+    let index = slideIndex + (isScrollUp ? 1 : -1);
+    if (index < 0) index = 0;
+    if (index > maxSlideCount - 1) index = maxSlideCount - 1;
+
     setSlideIndex(index);
-      
-    setTimeout(() => {
-      seIsAnimationOngoing(0);
-    }, (index === maxSlideCount || index === 0) ? 1 : DURATION);
+
+    if (index !== slideIndex) {
+      setSlideData({
+        ...slideData,
+        activeSlide: index,
+        isLastScrollUp: isScrollUp
+      });
+    }
+
+    setTimeout(
+      () => seIsAnimationOngoing(0),
+      index === maxSlideCount || index === 0 ? 1 : DURATION
+    );
   }
 
   return (
     <div className={`${styles.container}`} onWheel={showSlidesOnScroll}>
       <HomeHeader />
 
-      {[...Array(maxSlideCount + 1).fill(null)].map((v, i) => {
+      <SlideIndicator
+        count={maxSlideCount}
+        activeSlide={slideIndex}
+        showSlidesOnScroll={showSlidesOnScroll}
+      />
+
+      {[...Array(maxSlideCount).fill(null)].map((v, i) => {
         const { activeSlide, isLastScrollUp } = slideData;
         let count = isLastScrollUp ? 1 : -1;
         let nextSlide = activeSlide + count;
