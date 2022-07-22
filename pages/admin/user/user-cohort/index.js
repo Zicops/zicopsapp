df716f3e7a4414538e23c3a63aa1f5210223b906
shawnkit70 <71093268+shawnkit70@users.noreply.github.com>
2022-07-22 23:32:52 +0530

    removed unused imports

--- pages/admin/user/user-cohort/index.js
+++ pages/admin/user/user-cohort/index.js
@@ -2,22 +2,17 @@ import AdminHeader from '@/components/common/AdminHeader';
 import MainBody from '@/components/common/MainBody';
 import MainBodyBox from '@/components/common/MainBodyBox';
 import { userSideBarData } from '@/components/common/Sidebar/Logic/sidebar.helper';
-import { useState } from 'react';
-import ChortMasterTab from '@/components/UserComps/UserCohorts/ChortMasterTab';
+
 import Sidebar from '@/components/common/Sidebar';
 import UserCohorts from '@/components/UserComps/UserCohorts';
 
 const UserCohort = () => {
-  const [userIds, setUserIds] = useState([]);
-  const [userType, setUserType] = useState('Internal');
   return (
     <>
       <Sidebar sidebarItemsArr={userSideBarData} />
       <MainBody>
         <AdminHeader title="User Cohorts" isAddShown={true} pageRoute="/admin/users/user-cohort" />
         <MainBodyBox>
-          {/* <MyUser getUser={(list) => setUserIds(list)} /> */}
-          {/* <ChortMasterTab /> */}
           <UserCohorts />
         </MainBodyBox>
       </MainBody>
