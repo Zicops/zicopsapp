2c923f882fcd4c72fcaf1808f13ca9fef4ff9777
VAJRESH <vajresh005@gmail.com>
2022-07-26 18:30:17 +0530

    notes close and pin removed from notes tab

--- components/CourseBody/CourseBodyNotes/index.js
+++ components/CourseBody/CourseBodyNotes/index.js
@@ -1,26 +1,20 @@
+import LearnerPageContainer from '@/components/common/LearnerPageContainer';
+import LineText from '@/components/common/LineText';
+import useHandleNotes from '@/components/CustomVideoPlayer/Logic/useHandleNotes';
+import NoteCard from '@/components/CustomVideoPlayer/UiComponents/Notes/NoteCard';
+import { FloatingNotesAtom } from '@/state/atoms/notes.atom';
 import { useContext } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { filterAndSortTopicsBasedOnModuleId, filterModule } from '../../../helper/data.helper';
 import { ModuleAtom, TopicAtom } from '../../../state/atoms/module.atoms';
 import { courseContext } from '../../../state/contexts/CourseContext';
 import Dropdown from '../../common/Dropdown';
-import ItemSlider from '../../ItemSlider';
-import Notes from '../../Notes';
-import Bookmark from '../../slComponents/Bookmark';
-import { userContext } from '../../../state/contexts/UserContext';
+import styles from '../courseBody.module.scss';
 import Header from '../Header';
 import useShowData from '../Logic/useShowData';
 import TopicFileSlider from '../TopicFileSlider';
-import LineText from '@/components/common/LineText';
-import LearnerPageContainer from '@/components/common/LearnerPageContainer';
-import { FloatingNotesAtom } from '@/state/atoms/notes.atom';
-import NoteCard from '@/components/CustomVideoPlayer/UiComponents/Notes/NoteCard';
-import useHandleNotes from '@/components/CustomVideoPlayer/Logic/useHandleNotes';
-import styles from '../courseBody.module.scss';
 
 export default function CourseBodyNotes() {
-  const { bookmarkData, notes } = useContext(userContext);
-
   const courseContextData = useContext(courseContext);
   const {
     getModuleOptions,
@@ -75,24 +69,20 @@ export default function CourseBodyNotes() {
           <LearnerPageContainer>
             <div className={`${styles.notesCardsContainer}`}>
               <div className={`${styles.addNote}`} onClick={addNewNote}>
-                + Add Note
+                <span>+</span>
+                Add Note
               </div>
 
               {floatingNotes
-                .map((noteObj, i) => {
-                  if (!noteObj.isOpen) return null;
-
-                  return (
-                    <NoteCard
-                      key={noteObj.id}
-                      handleClose={() => handleClose(noteObj)}
-                      handlePin={(e) => handlePin(e, noteObj)}
-                      handleDelete={() => deleteNote(noteObj)}
-                      handleNote={handleNote}
-                      noteObj={noteObj}
-                    />
-                  );
-                })
+                .map((noteObj, i) => (
+                  <NoteCard
+                    key={noteObj.id}
+                    handleDelete={() => deleteNote(noteObj)}
+                    handleNote={handleNote}
+                    noteObj={noteObj}
+                    isDraggable={false}
+                  />
+                ))
                 .reverse()}
             </div>
           </LearnerPageContainer>
