be2477762543038a5e96a57a20045f2cf1f1c8e3
shawnkit70 <71093268+shawnkit70@users.noreply.github.com>
2022-07-27 15:50:02 +0530

    added atoms to user About

--- components/LearnerUserProfile/SingleInfo/index.js
+++ components/LearnerUserProfile/SingleInfo/index.js
@@ -1,14 +1,31 @@
 import LabeledInput from '@/components/common/FormComponents/LabeledInput';
+import { UserStateAtom } from '@/state/atoms/users.atom';
 import { useState } from 'react';
+import { useRecoilState } from 'recoil';
 import styles from '../learnerUserProfile.module.scss';
 
 const SingleInfo = ({ userData, isEditable = true }) => {
   const [tempData, setTempData] = useState(userData.info);
+  const [userDataMain, setUserDataMain] = useRecoilState(UserStateAtom);
+
+  switch (tempData) {
+    case 'Name':
+      setTempData(
+        userDataMain ? `${userDataMain.first_name} ${userDataMain?.last_name}` : tempData
+      );
+      break;
+    case 'Email':
+      setTempData(userDataMain ? userDataMain?.email : tempData);
+      break;
+    case 'Contact':
+      setTempData(userDataMain ? userDataMain?.phone : tempData);
+      break;
+  }
 
   return (
     <div className={`${styles.singleWraper}`}>
       <div className={`${styles.singleImage}`}>
-        <img src={userData.image} />
+        <img src={userData.image || userDataMain?.photo_url} />
       </div>
       <div className={`${styles.textWraper}`}>
         <div className={`${styles.smallText}`}>{userData.text}:</div>
--- components/LearnerUserProfile/UserHead/index.js
+++ components/LearnerUserProfile/UserHead/index.js
@@ -1,19 +1,34 @@
+import { UserStateAtom } from '@/state/atoms/users.atom';
+import { useRecoilState } from 'recoil';
 import styles from '../learnerUserProfile.module.scss';
 const UserHead = () => {
+  function truncateTo16(str) {
+    return str.length > 16 ? str.substring(0, 13) + '...' : str;
+  }
+  const [userProfileData, setUserProfileData] = useRecoilState(UserStateAtom);
   return (
     <div className={`${styles.userHead}`}>
       <div className={`${styles.editIcon}`}>
         <img src="/images/svg/edit.svg" />
       </div>
-
-      <div className={`${styles.userImage}`}>
-        <img src="/images/profile_picture.png" />
+      <div className={`${styles.userImageContainer}`}>
+        <div className={`${styles.userImage}`}>
+          <img
+            src={`${
+              userProfileData?.photo_url ? userProfileData?.photo_url : '/images/swagDP.jpg'
+            }`}
+          />
+        </div>
         <div className={`${styles.editFillIcon}`}>
           <img src="/images/svg/edit-black.svg" />
         </div>
       </div>
 
-      <div className={`${styles.userName}`}>Aakash Chakraborty</div>
+      <div className={`${styles.userName}`}>
+        {userProfileData?.first_name
+          ? `${userProfileData?.first_name} ${userProfileData?.last_name}`
+          : 'Aakash Chakraborty'}
+      </div>
       <div className={`${styles.userRole}`}>Learning manager at accenture</div>
     </div>
   );
