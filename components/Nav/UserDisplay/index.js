42f57a69de5f373d0def835e960238353c037043
shawnkit70 <71093268+shawnkit70@users.noreply.github.com>
2022-07-27 15:50:27 +0530

    navBar changes

--- components/Nav/UserDisplay/index.js
+++ components/Nav/UserDisplay/index.js
@@ -10,13 +10,16 @@ const UserDisplay = () => {
   return (
     <>
       <div className={styles.profile}>
-        <img
-          className={styles.profilepic}
-          src={`${userProfileData ? userProfileData?.photo_url : '/images/dp.png'}`}
-        />
+        <div className={styles.profilepic}>
+          <img
+            src={`${
+              userProfileData?.photo_url ? userProfileData?.photo_url : '/images/swagDP.jpg'
+            }`}
+          />
+        </div>
         <div className={styles.profilename}>
           <div className={styles.name}>
-            {userProfileData
+            {userProfileData?.first_name
               ? truncateTo16(`${userProfileData?.first_name} ${userProfileData?.last_name}`)
               : truncateTo16('Abhishek Ghosh')}
           </div>
