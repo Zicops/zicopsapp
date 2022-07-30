2386fba2d503e36d5dff3ca0389b676db807eee3
shawnkit70 <71093268+shawnkit70@users.noreply.github.com>
2022-07-30 12:11:10 +0530

    added password validation

--- helper/common.helper.js
+++ helper/common.helper.js
@@ -44,3 +44,21 @@ export function changeHandler(e, state, setState, inputName = null) {
 export function isEmail(email) {
   return /[\w\d\.-]+@[\w\d\.-]+\.[\w\d\.-]+/.test(email);
 }
+
+//Password validation
+export function isPassword(password) {
+  let errors = [];
+  if (password.length < 8) {
+    errors.push('Your password must be at least 8 characters');
+  }
+  if (password.search(/[a-z]/i) < 0) {
+    errors.push('Your password must contain at least one letter.');
+  }
+  if (password.search(/[0-9]/) < 0) {
+    errors.push('Your password must contain at least one digit.');
+  }
+  if (errors.length > 0) {
+    return errors;
+  }
+  return true;
+}
