2f5a7f143a819a021a2b8d50e1c3bfa6ce963702
shawnkit70 <71093268+shawnkit70@users.noreply.github.com>
2022-08-02 18:53:20 +0530

    added  handleClick in UserInforWrapper

--- components/LearnerUserProfile/UserInfoWraper/index.js
+++ components/LearnerUserProfile/UserInfoWraper/index.js
@@ -1,11 +1,22 @@
 import Button from '@/components/common/Button';
+import { IsUpdatedAtom } from '@/state/atoms/users.atom';
+import { useRecoilState } from 'recoil';
 import styles from '../learnerUserProfile.module.scss';
 import SingleInfo from '../SingleInfo';
 // import SingleUserDetail from '../SingleUserDetail';
 // import { userData } from '../Logic/userData.helper.js';
-const UserInfoWraper = ({ userData, isEditable, toggleEditable }) => {
-  // console.log(userData);
-
+const UserInfoWraper = ({
+  userData,
+  isEditable,
+  toggleEditable,
+  handleUpdate = function () {}
+}) => {
+  const [isUpdate, setIsUpdate] = useRecoilState(IsUpdatedAtom);
+  function handleClick() {
+    handleUpdate();
+    toggleEditable();
+    setIsUpdate(true);
+  }
   return (
     <div className={`${styles.userInfoWraper}`}>
       {userData.map((v) => (
@@ -14,7 +25,7 @@ const UserInfoWraper = ({ userData, isEditable, toggleEditable }) => {
 
       {isEditable && (
         <div>
-          <Button text={'Update'} clickHandler={toggleEditable} styleClass={styles.updateBtn} />
+          <Button text={'Update'} clickHandler={handleClick} styleClass={styles.updateBtn} />
           {/* <Button text={'Cancel'} clickHandler={toggleEditable} /> */}
         </div>
       )}
