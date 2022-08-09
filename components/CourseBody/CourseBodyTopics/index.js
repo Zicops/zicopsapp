3e7801427fbd25126b29e3b390ca2d62ed27315f
VAJRESH <vajresh005@gmail.com>
2022-08-09 16:38:41 +0530

    chapter 1 duplicate prevented on render

--- components/CourseBody/CourseBodyTopics/index.js
+++ components/CourseBody/CourseBodyTopics/index.js
@@ -68,7 +68,7 @@ export default function CourseBodyTopics() {
               name={chapter.name}
               description={chapter.description}
               chapterIndex={index + 1}
-              key={chapter.name}
+              key={chapter.id}
               chapterId={chapter.id}
               moduleId={selectedModule.value}
               getModuleOptions={getModuleOptions}
@@ -81,7 +81,7 @@ export default function CourseBodyTopics() {
 
             return (
               <TopicBox
-                key={topic.name}
+                key={topic.id}
                 topicCount={index + 1}
                 topic={topic}
                 topicIndex={++topicIndex}
