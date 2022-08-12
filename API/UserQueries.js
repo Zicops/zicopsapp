f11e0911385d0db0c9882ed3da58796379145a88
shawnkit70 <71093268+shawnkit70@users.noreply.github.com>
2022-08-13 03:01:18 +0530

    added userQuieries

--- API/UserQueries.js
+++ API/UserQueries.js
@@ -39,6 +39,27 @@ export const userQueryClient = new ApolloClient({
   cache: new InMemoryCache()
 });
 
+export const GET_USER_DETAIL = gql`
+  query GetUserDetails($user_id: String!) {
+    getUserDetails(user_id: $user_id) {
+      id
+      first_name
+      last_name
+      status
+      role
+      is_verified
+      is_active
+      gender
+      created_by
+      updated_by
+      created_at
+      updated_at
+      email
+      phone
+      photo_url
+    }
+  }
+`;
 export const GET_USERS_FOR_ADMIN = gql`
   query GetUsersForAdmin($publish_time: Int, $pageCursor: String, $pageSize: Int) {
     getUsersForAdmin(
@@ -97,6 +118,106 @@ export const GET_USER_COURSE_MAPS = gql`
       pageCursor
       direction
       pageSize
+    }
+  }
+`;
+
+export const GET_USER_ORGANIZATIONS = gql`
+  query GetUserOrganization {
+    getUserOrganizations {
+      user_organization_id
+      user_id
+      user_lsp_id
+      organization_id
+      organization_role
+      is_active
+      employee_id
+      created_by
+      updated_by
+      created_at
+      updated_at
+    }
+  }
+`;
+
+export const GET_USER_ORGANIZATION_DETAIL = gql`
+  query GetUserOrgDetails($user_id: String!, $user_lsp_id: String!) {
+    getUserOrgDetails(user_id: $user_id, user_lsp_id: $user_lsp_id) {
+      user_organization_id
+      user_id
+      user_lsp_id
+      organization_id
+      organization_role
+      is_active
+      employee_id
+      created_by
+      updated_by
+      created_at
+      updated_at
+    }
+  }
+`;
+
+export const GET_USER_LEARNINGSPACES = gql`
+  query GetUserLsps {
+    getUserLsps {
+      user_lsp_id
+      user_id
+      lsp_id
+      status
+      created_by
+      updated_by
+      created_at
+      updated_at
+    }
+  }
+`;
+
+export const GET_USER_LEARNINGSPACES_DETAILS = gql`
+  query GetUserLspByLspId($user_id: String!, $user_lsp_id: String!) {
+    getUserLspByLspId(user_id: $user_id, lsp_id: $user_lsp_id) {
+      user_lsp_id
+      user_id
+      lsp_id
+      status
+      created_by
+      updated_by
+      created_at
+      updated_at
+    }
+  }
+`;
+
+export const GET_USER_PREFERENCES = gql`
+  {
+    getUserPreferences {
+      user_preference_id
+      user_id
+      user_lsp_id
+      sub_category
+      is_base
+      is_active
+      created_by
+      updated_by
+      created_at
+      updated_at
+    }
+  }
+`;
+
+export const GET_USER_PREFERENCES_DETAILS = gql`
+  query GetUserPreferenceForLsp($user_id: String!, $user_lsp_id: String!) {
+    getUserPreferenceForLsp(user_id: $user_id, user_lsp_id: $user_lsp_id) {
+      user_preference_id
+      user_id
+      user_lsp_id
+      sub_category
+      is_base
+      is_active
+      created_by
+      updated_by
+      created_at
+      updated_at
     }
   }
 `;
