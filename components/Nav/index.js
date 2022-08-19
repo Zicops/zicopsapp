ec788f02bf0a654fb00858c56f479bbc3c33a10d
VAJRESH <vajresh005@gmail.com>
2022-08-19 11:43:41 +0530

    switch to admin in nav updated

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -55,7 +55,7 @@ export default function Nav() {
           navmenuicon={HamburgerMenuIcon}
         />
 
-        <Link href={isAdmin ? '/admin' : '/'}>
+        <Link href={!isOnLearnerSide ? '/admin' : '/'}>
           <a className={styles.logo}>
             <img src="/images/zicops-header-logo.png" />
           </a>
