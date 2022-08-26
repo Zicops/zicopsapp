a237837bac2ee9c6730220884a1b6477dc65b7e6
VAJRESH <vajresh005@gmail.com>
2022-08-27 01:31:11 +0530

    subtitles load workaround added

--- components/CustomVideoPlayer/VideoPlayer/index.js
+++ components/CustomVideoPlayer/VideoPlayer/index.js
@@ -17,6 +17,43 @@ export default function VideoPlayer({
   const [subtitles, setSubtitles] = useState('');
 
   useEffect(() => {
+    if (!isSubtitleShown) return;
+
+    function handleSubtitleLoad() {
+      if (
+        window.navigator.userAgent.indexOf('MSIE ') < 0 &&
+        window.navigator.userAgent.indexOf('Trident/') < 0
+      ) {
+        // Not IE, do nothing.
+        return;
+      }
+
+      var tracks = document.querySelectorAll('track');
+
+      for (var i = 0; i < tracks.length; i++) {
+        loadTrackWithAjax(tracks[i]);
+      }
+    }
+
+    function loadTrackWithAjax(track) {
+      var xhttp = new XMLHttpRequest();
+
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200 && this.responseText) {
+          // If VTT fetch succeeded, replace the src with a BLOB URL.
+          var blob = new Blob([this.responseText], { type: 'text/vtt' });
+          track.setAttribute('src', URL.createObjectURL(blob));
+        }
+      };
+      xhttp.open('GET', track.src, true);
+      xhttp.send();
+    }
+
+    window.addEventListener('load', handleSubtitleLoad);
+    return () => window.removeEventListener('keyup', handleSubtitleLoad);
+  }, []);
+
+  useEffect(() => {
     if (!videoElement?.current) return;
     if (!isSubtitleShown) return;
 
