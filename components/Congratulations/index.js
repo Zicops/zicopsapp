294b22a01ce078ef227828d4cdbdbbdc35d56b77
VAJRESH <vajresh005@gmail.com>
2022-08-29 16:43:02 +0530

    exit and return to main screen route added

--- components/Congratulations/index.js
+++ components/Congratulations/index.js
@@ -1,6 +1,8 @@
 import { LearnerExamAtom } from '@/state/atoms/exams.atoms';
-import { UserExamDataAtom } from '@/state/atoms/video.atom';
+import { UserStateAtom } from '@/state/atoms/users.atom';
+import { UserCourseDataAtom, UserExamDataAtom } from '@/state/atoms/video.atom';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import AttemptsTable from '../common/AttemptsTable';
 import CongratulationsScreenButton from '../common/CongratulationsScreenButton';
@@ -11,10 +13,24 @@ import { data, getResultStyles } from './Logic/congratulationsHead.helper';
 const Congratulations = ({ resultIndex }) => {
   const learnerExamData = useRecoilValue(LearnerExamAtom);
   const userExamData = useRecoilValue(UserExamDataAtom);
+  const userCourseData = useRecoilValue(UserCourseDataAtom);
+  const userData = useRecoilValue(UserStateAtom);
 
   const router = useRouter();
   const cpId = router?.query?.cpId;
   const examId = router?.query?.examId;
+
+  useEffect(() => {
+    if (!userExamData?.currentAttemptId) router.back();
+  }, []);
+
+  const currentAttemptData = userExamData?.userExamAttempts.find(
+    (ea) => ea?.user_ea_id === userExamData?.currentAttemptId
+  );
+  function handleReturnToMainScreen() {
+    const courseId = userCourseData?.userCourseMapping?.course_id;
+    router.push(`/course/${courseId}?activateExam=${examId}`, `/course/${courseId}`);
+  }
 
   const style = getResultStyles(data[resultIndex].result);
 
@@ -22,7 +38,7 @@ const Congratulations = ({ resultIndex }) => {
     <div style={{ width: '70%' }}>
       <CongratulationsScreen>
         <CongratulationsHead
-          user_name={'pallav'}
+          user_name={`${userData?.first_name || ''} ${userData?.last_name || ''}`}
           exam_name={learnerExamData?.examData?.name || ''}
           data={data[resultIndex]}
           style={style}
@@ -31,7 +47,7 @@ const Congratulations = ({ resultIndex }) => {
           totalAttempts={3}
           attemptData={[
             {
-              attempt: 1,
+              attempt: currentAttemptData?.attempt_no || 1,
               examScore: learnerExamData?.resultData?.examScore,
               totalMarks: learnerExamData?.examData?.totalMarks,
               result: data[resultIndex].result
@@ -47,7 +63,10 @@ const Congratulations = ({ resultIndex }) => {
           title={'View Attempt History'}
         />
 
-        <CongratulationsScreenButton title={'Exit And Return To Main Screen'} />
+        <CongratulationsScreenButton
+          title={'Exit And Return To Main Screen'}
+          handleClick={handleReturnToMainScreen}
+        />
       </CongratulationsFooter>
     </div>
   );
