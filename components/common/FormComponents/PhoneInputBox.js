29470e23cb36010252bd3e4c5bb35c28c3a3cfdb
shawnkit70 <71093268+shawnkit70@users.noreply.github.com>
2022-08-31 04:19:46 +0530

    made width dynamic for phoneInputbox

--- components/common/FormComponents/PhoneInputBox.js
+++ components/common/FormComponents/PhoneInputBox.js
@@ -2,12 +2,12 @@ import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
 import styles from './formComponents.module.scss';
 
-export default function PhoneInputBox({ changeHandler, value }) {
+export default function PhoneInputBox({ changeHandler, value, isLabel = true }) {
   return (
     <>
       <PhoneInput
         containerClass={`${styles.labeledInputWrapper}`}
-        containerStyle={{ width: '75%' }}
+        containerStyle={isLabel ? { width: '75%' } : { width: '100%' }}
         inputClass={`${styles.inputStyles}`}
         inputStyle={{ width: '100%', paddingLeft: '50px' }}
         buttonClass={`${styles.countryCodeBtn}`}
