7c5b5037a164240cfd511bc3aec4d942fc365db2
VAJRESH <vajresh005@gmail.com>
2022-09-13 15:48:17 +0530

    search functionality added

--- components/Search/index.js
+++ components/Search/index.js
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import useHandleSearch from './Logic/useHandleSearch';
 import SearchBody from './SearchBody';
 import SearchBookmarks from './SearchBookmarks';
@@ -5,6 +6,8 @@ import SearchHeader from './SearchHeader';
 
 export default function Search() {
   const { courses, lastItemRef, filters, setFilters, clearAllFilters } = useHandleSearch();
+  const router = useRouter();
+  const searchQuery = router.query?.searchQuery || '';
 
   return (
     <div
@@ -15,7 +18,30 @@ export default function Search() {
       }}>
       <SearchHeader filters={filters} setFilters={setFilters} clearAllFilters={clearAllFilters} />
       <SearchBookmarks />
-      <SearchBody courses={courses} lastItemRef={lastItemRef} />
+      <SearchBody
+        courses={courses?.filter((course) => {
+          const nameFilter = course?.name?.toLowerCase()?.includes(searchQuery?.toLowerCase());
+          let langFilter = true,
+            catFilter = true,
+            subCatFilter = true,
+            typeFilter = true;
+
+          if (filters?.lang) langFilter = course?.language?.includes(filters?.lang);
+          if (filters?.category) catFilter = course?.category?.includes(filters?.category);
+          if (filters?.subCategory)
+            subCatFilter =
+              course?.sub_categories?.findIndex((subCat) =>
+                subCat?.name?.includes(filters?.subCategory)
+              ) > 0;
+          if (filters?.type) typeFilter = course?.type?.includes(filters?.type);
+
+          if (nameFilter && langFilter && catFilter && subCatFilter && typeFilter)
+            console.log(course);
+
+          return nameFilter && langFilter && catFilter && subCatFilter && typeFilter;
+        })}
+        lastItemRef={lastItemRef}
+      />
     </div>
   );
 }
