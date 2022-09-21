2433ed5cc637ab5dc8f427c006fa0acd873d8eb4
shawnkit70 <71093268+shawnkit70@users.noreply.github.com>
2022-09-21 23:10:33 +0100

    Update index.js

--- components/AuthChecker/index.js
+++ components/AuthChecker/index.js
@@ -6,23 +6,23 @@ const AuthChecker = ({ children }) => {
   const router = useRouter();
   const [authorized, setAuthorized] = useState(true);
 
-  // useEffect(() => {
-  //   // on initial load - run auth check
-  //   authCheck(router.asPath);
+useEffect(() => {
+    // on initial load - run auth check
+    authCheck(router.asPath);
 
-  //   // on route change start - hide page content by setting authorized to false
-  //   const hideContent = () => setAuthorized(false);
-  //   router.events.on('routeChangeStart', hideContent);
+    // on route change start - hide page content by setting authorized to false
+    const hideContent = () => setAuthorized(false);
+    router.events.on('routeChangeStart', hideContent);
 
-  //   // on route change complete - run auth check
-  //   router.events.on('routeChangeComplete', authCheck);
+    // on route change complete - run auth check
+    router.events.on('routeChangeComplete', authCheck);
 
-  //   // unsubscribe from events in useEffect return function
-  //   return () => {
-  //     router.events.off('routeChangeStart', hideContent);
-  //     router.events.off('routeChangeComplete', authCheck);
-  //   };
-  // }, []);
+    // unsubscribe from events in useEffect return function
+    return () => {
+      router.events.off('routeChangeStart', hideContent);
+      router.events.off('routeChangeComplete', authCheck);
+    };
+  }, []);
 
   function authCheck(url) {
     // redirect to login page if accessing a private page and not logged in
