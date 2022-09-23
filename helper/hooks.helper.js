483ace3952c2aec0a51466c92a3924779b11d9c9
shawnkit70 <ankitjha844@gmail.com>
2022-09-23 13:10:16 +0100

    add common hook for user courses progress

--- helper/hooks.helper.js
+++ helper/hooks.helper.js
@@ -1,71 +1,150 @@
+import { GET_COURSE } from '@/api/Queries';
 import {
     UPDATE_USER, userClient
 } from '@/api/UserMutations';
+import { GET_USER_COURSE_MAPS, GET_USER_COURSE_PROGRESS } from '@/api/UserQueries';
 import { ToastMsgAtom } from '@/state/atoms/toast.atom';
 import {
     getUserObject, UserStateAtom
 } from '@/state/atoms/users.atom';
 import { useMutation } from '@apollo/client';
+import moment from 'moment';
 import { useEffect, useState } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
+import { loadQueryDataAsync } from './api.helper';
+import { getCurrentEpochTime } from './common.helper';
 
-export default function useHandleUserDetails() {
-  const [updateAbout, { error: createError }] = useMutation(UPDATE_USER, {
-    client: userClient
-  });
+// export default function useHandleUserDetails() {
+//   const [updateAbout, { error: createError }] = useMutation(UPDATE_USER, {
+//     client: userClient
+//   });
   
-  //recoil states
-  const userDataAbout = useRecoilValue(UserStateAtom);
+//   //recoil states
+//   const userDataAbout = useRecoilValue(UserStateAtom);
  
-  const [toastMsg, setToastMsg] = useRecoilState(ToastMsgAtom);
+//   const [toastMsg, setToastMsg] = useRecoilState(ToastMsgAtom);
 
-  // local state
-  const [userAboutData, setUserAboutData] = useState(getUserObject());
+//   // local state
+//   const [userAboutData, setUserAboutData] = useState(getUserObject());
  
 
-  // setting up local states
-  useEffect(() => {
-    setUserAboutData(getUserObject(userDataAbout));
+//   // setting up local states
+//   useEffect(() => {
+//     setUserAboutData(getUserObject(userDataAbout));
     
-  }, [userData]);
+//   }, [userData]);
 
 
-  async function updateAboutUser() {
-    const sendUserData = {
-    id: userAboutData?.id,
-      first_name: userAboutData?.first_name,
-      last_name: userAboutData?.last_name,
+//   async function updateAboutUser() {
+//     const sendUserData = {
+//     id: userAboutData?.id,
+//       first_name: userAboutData?.first_name,
+//       last_name: userAboutData?.last_name,
 
-      status: userAboutData?.status,
-      role: userAboutData?.role || 'Learner',
-      email: userAboutData?.email,
-      phone: userAboutData?.phone,
-      photo_url: userAboutData?.photo_url || null,
-      Photo: userAboutData?.photo || null,
-      gender: userAboutData?.gender,
+//       status: userAboutData?.status,
+//       role: userAboutData?.role || 'Learner',
+//       email: userAboutData?.email,
+//       phone: userAboutData?.phone,
+//       photo_url: userAboutData?.photo_url || null,
+//       Photo: userAboutData?.photo || null,
+//       gender: userAboutData?.gender,
 
-      is_verified: true,
-      is_active: true,
+//       is_verified: true,
+//       is_active: true,
 
-      created_by: userAboutData?.created_by || 'Zicops',
-      updated_by: userAboutData?.updated_by || 'Zicops'
-    };
+//       created_by: userAboutData?.created_by || 'Zicops',
+//       updated_by: userAboutData?.updated_by || 'Zicops'
+//     };
 
-    console.log(sendUserData, 'updateAboutUser');
+//     console.log(sendUserData, 'updateAboutUser');
 
-    let isError = false;
-    const res = await updateAbout({ variables: sendUserData }).catch((err) => {
-      console.log(err);
-      isError = !!err;
-      return setToastMsg({ type: 'danger', message: 'Update User Error' });
-    });
+//     let isError = false;
+//     const res = await updateAbout({ variables: sendUserData }).catch((err) => {
+//       console.log(err);
+//       isError = !!err;
+//       return setToastMsg({ type: 'danger', message: 'Update User Error' });
+//     });
 
-    console.log(res);
+//     console.log(res);
+//   }
+
+//   return { updateAboutUser };
+// }
+
+export default function useUserCourseData(){
+
+  async function getUserCourseProgress(currentUserId=null){
+    if(!currentUserId) return setToastMsg({type:'danger' , message:'Need to provide user id for course progress!'});
+    
+    const assignedCoursesRes = await loadQueryDataAsync(
+      GET_USER_COURSE_MAPS,
+      {
+        user_id: currentUserId,
+        publish_time: getCurrentEpochTime(),
+        pageCursor: '',
+        pageSize: 999999999
+      },
+      {},
+      userClient
+    );
+
+    if (assignedCoursesRes?.error)
+      return setToastMsg({ type: 'danger', message: 'Course Maps Load Error' });
+    const assignedCoursesToUser = assignedCoursesRes?.getUserCourseMaps?.user_courses;
+
+    const allAssignedCourses = [];
+    for (let i = 0; i < assignedCoursesToUser.length; i++) {
+      const courseMap = assignedCoursesToUser[i];
+      const mapId = courseMap?.user_course_id;
+      const course_id = courseMap?.course_id;
+
+      const courseProgressRes = await loadQueryDataAsync(
+        GET_USER_COURSE_PROGRESS,
+        { userId: currentUserId, userCourseId: mapId },
+        {},
+        userClient
+      );
+
+      if (courseProgressRes?.error) {
+        setToastMsg({ type: 'danger', message: 'Course Progress Load Error' });
+        continue;
+      }
+      const userProgressArr = courseProgressRes?.getUserCourseProgressByMapId;
+
+      // if (!userProgressArr?.length) continue;
+
+      let topicsStarted = 0;
+      userProgressArr?.map((topic) => {
+        if (topic?.status !== 'not-started') ++topicsStarted;
+      });
+      console.log(topicsStarted);
+      const courseProgress = userProgressArr?.length
+        ? Math.floor((topicsStarted * 100) / userProgressArr?.length)
+        : 0;
+
+      const courseRes = await loadQueryDataAsync(GET_COURSE, { course_id: course_id });
+      if (courseRes?.error) {
+        setToastMsg({ type: 'danger', message: 'Course Load Error' });
+        continue;
+      }
+
+      const added_by = JSON.parse(assignedCoursesToUser[i]?.added_by);
+      allAssignedCourses.push({
+        ...courseRes?.getCourse,
+        completedPercentage: userProgressArr?.length ? courseProgress : '0',
+        added_by: added_by?.role,
+        created_at: moment.unix(assignedCoursesToUser[i]?.created_at).format('DD/MM/YYYY'),
+        expected_completion: moment.unix(assignedCoursesToUser[i]?.end_date).format('DD/MM/YYYY')
+      });
+    }
+
+    const userCourses = allAssignedCourses.filter(
+      (v, i, a) => a.findIndex((v2) => v2?.id === v?.id) === i
+    );
+
+    if(!userCourses?.length) return setToastMsg({ type: 'info', message: 'No courses found!' })
+
+    return userCourses ;
   }
-
-  return { updateAboutUser };
-}
-
-export default function useAddUserDetails() {
-    
+  return {getUserCourseProgress}
 }
