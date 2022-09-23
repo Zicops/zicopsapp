e10942141d65129edc96bf7c8233abe6f5228e58
shawnkit70 <ankitjha844@gmail.com>
2022-09-23 23:11:22 +0100

    added dynamic preferences and navigate to search page according to selected preferences

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,8 +1,8 @@
-import { GET_USER_DETAIL, userQueryClient } from '@/api/UserQueries';
+import { GET_USER_DETAIL, GET_USER_LEARNINGSPACES_DETAILS, userQueryClient } from '@/api/UserQueries';
 import { loadQueryDataAsync } from '@/helper/api.helper';
-import { HIDE_HEADER_FOOTER_FOR_ROUTE } from '@/helper/constants.helper';
+import { HIDE_HEADER_FOOTER_FOR_ROUTE, LEARNING_SPACE_ID } from '@/helper/constants.helper';
 import { getUserData } from '@/helper/loggeduser.helper';
-import { UserStateAtom } from '@/state/atoms/users.atom';
+import { getUserOrgObject, UsersOrganizationAtom, UserStateAtom } from '@/state/atoms/users.atom';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
@@ -12,6 +12,7 @@ import { main } from './layout.module.scss';
 
 export default function Layout({ children }) {
   const [userData, setUserData] = useRecoilState(UserStateAtom);
+  const [userOrgData, setUserOrgData] = useRecoilState(UsersOrganizationAtom);
 
   const [isFullHeight, setIsFullHeight] = useState(0);
   const router = useRouter();
@@ -35,6 +36,15 @@ export default function Layout({ children }) {
     const basicInfo = userData?.getUserDetails?.[0];
 
     setUserData({ ...userData, ...data, ...basicInfo });
+
+    const userLearningSpaceData =  await loadQueryDataAsync(GET_USER_LEARNINGSPACES_DETAILS,{user_id:userId,lsp_id:LEARNING_SPACE_ID},{},userQueryClient);
+    if(userLearningSpaceData?.error) return console.log('User lsp load error!');
+    // console.log(userLearningSpaceData,'learning space data');
+    //temporary solution only valid for one lsp...need to change later!
+    sessionStorage?.setItem('lspData',JSON.stringify(userLearningSpaceData?.getUserLspByLspId));
+    // console.log(userLearningSpaceData?.getUserLspByLspId?.user_lsp_id,'lsp')
+    setUserOrgData(getUserOrgObject({user_lsp_id:userLearningSpaceData?.getUserLspByLspId?.user_lsp_id}));
+    return;
   }, [userData?.id]);
 
   useEffect(() => {
