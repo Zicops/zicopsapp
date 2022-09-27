ac992690beed3a0e07bdf72d1b9e35947b653f1d
VAJRESH <vajresh005@gmail.com>
2022-09-27 19:06:31 +0530

    fix: sitemap updated
    display text added
    removed /admin/course page as it duplicate of /admin/course/my-courses

--- state/atoms/sitemap.atom.js
+++ state/atoms/sitemap.atom.js
@@ -3,21 +3,33 @@ import { atom, selector } from 'recoil';
 export const SiteMapAtom = atom({
   key: 'SiteMap',
   default: [
-    { head: 'course', route: '/admin/course' },
-    { head: 'course', route: '/admin/courses' },
-    { head: 'course', route: '/admin/course/categories' },
-    { head: 'course', route: '/admin/course/subcategories' },
-    { head: 'course', route: '/admin/course/zicops-courses' },
-    { head: 'exams', route: '/admin/exams/my-exams' },
-    { head: 'exams', route: '/admin/exams/my-exams/add' },
-    { head: 'exams', route: '/admin/exams/my-question-bank' },
-    { head: 'exams', route: '/admin/exams/my-question-papers' },
-    { head: 'exams', route: '/admin/exams/my-question-papers/add' },
-    { head: 'exams', route: '/admin/exams/question-bank' },
-    { head: 'exams', route: '/admin/exams/zicops-exam' },
-    { head: 'exams', route: '/admin/exams/zicops-question-bank' },
-    { head: 'exams', route: '/admin/exams/zicops-question-papers' },
-    { head: 'user', route: '/admin/user' }
+    { head: 'course', displayName: 'My Courses', route: '/admin/course/my-courses' },
+    { head: 'course', displayName: 'Add Course', route: '/admin/courses' },
+    { head: 'course', displayName: 'Categories', route: '/admin/course/categories' },
+    { head: 'course', displayName: 'Sub Categories', route: '/admin/course/subcategories' },
+    { head: 'course', displayName: 'Zicops Courses', route: '/admin/course/zicops-courses' },
+    { head: 'exams', displayName: 'My Exams', route: '/admin/exams/my-exams' },
+    { head: 'exams', displayName: 'Add Exam', route: '/admin/exams/my-exams/add' },
+    { head: 'exams', displayName: 'My Question Bank', route: '/admin/exams/my-question-bank' },
+    { head: 'exams', displayName: 'My Question Paper', route: '/admin/exams/my-question-papers' },
+    {
+      head: 'exams',
+      displayName: 'Add Question Paper',
+      route: '/admin/exams/my-question-papers/add'
+    },
+    { head: 'exams', displayName: 'My Question Bank', route: '/admin/exams/question-bank' },
+    { head: 'exams', displayName: 'Zicops Exams', route: '/admin/exams/zicops-exam' },
+    {
+      head: 'exams',
+      displayName: 'Zicops Question Bank',
+      route: '/admin/exams/zicops-question-bank'
+    },
+    {
+      head: 'exams',
+      displayName: 'Zicops Question Papers',
+      route: '/admin/exams/zicops-question-papers'
+    },
+    { head: 'user', displayName: 'My Users', route: '/admin/user/my-users' }
   ]
 });
 
@@ -30,7 +42,7 @@ export const getSiteMapForView = selector({
     sitemap.forEach((item) => {
       if (!sitemapForView[item.head]) sitemapForView[item.head] = [];
 
-      sitemapForView[item.head].push(item.route);
+      sitemapForView[item.head].push(item);
     });
 
     return sitemapForView;
