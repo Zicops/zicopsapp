c9996b007c20b1dd2cfd88798d66c1fd99b8c1a6
Jayanta Sarkar <jtas.sarkar@gmail.com>
2022-09-29 02:12:05 +0530

    scrollbar added in leaderboard

--- components/LearnerUserProfile/UserCohortTab/CohortPopUp/LeaderboardTab.js
+++ components/LearnerUserProfile/UserCohortTab/CohortPopUp/LeaderboardTab.js
@@ -1,6 +1,34 @@
 import SearchBar from '@/components/common/FormComponents/SearchBar';
 import styles from '../../learnerUserProfile.module.scss';
 
+function LeaderboardCard({user}) {
+  return (
+    <div className={`${styles.leaderboardBody}`}>
+      <div className={`${styles.profile}`}>
+        <img src="/images/user_profile_circle.png" alt="" />
+        <div className={`${styles.name}`}>{user?.name}</div>
+      </div>
+      <div className={`${styles.brankText}`}>{user?.bestRank}</div>
+      <div className={`${styles.crankBlock}`}>
+        <div className={`${styles.crankImg}`}>
+          {user?.isTopThree ? (
+            <img src="/images/svg/workspace_premium.svg" alt="" />
+          ) : (
+            <img src="/images/svg/award-line 1.svg" alt="" />
+          )}
+        </div>
+        {user?.currentRank}
+        <div className={`${styles.crankArrow}`}>
+          {user?.rankGained ? (
+            <img src="/images/svg/arrow_drop_up.svg" alt="" />
+          ) : (
+            <img src="/images/svg/arrow_drop_down.svg" alt="" />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
 export default function LeaderboardTab() {
   const user = {
     name: 'Sonali Kokalki',
@@ -16,7 +44,7 @@ export default function LeaderboardTab() {
           inputDataObj={{
             inputOptions: {
               inputName: 'filter',
-              placeholder: 'Search Courses',
+              placeholder: 'Search Courses'
             }
           }}
         />
@@ -26,29 +54,17 @@ export default function LeaderboardTab() {
         <div className={`${styles.brank}`}>Best Rank</div>
         <div className={`${styles.crank}`}>Current Rank</div>
       </div>
-      <div className={`${styles.leaderboardBody}`}>
-        <div className={`${styles.profile}`}>
-          <img src="/images/user_profile_circle.png" alt="" />
-          <div className={`${styles.name}`}>{user?.name}</div>
-        </div>
-        <div className={`${styles.brankText}`}>{user?.bestRank}</div>
-        <div className={`${styles.crankBlock}`}>
-          <div className={`${styles.crankImg}`}>
-            {user?.isTopThree ? (
-              <img src="/images/svg/workspace_premium.svg" alt="" />
-            ) : (
-              <img src="/images/svg/award-line 1.svg" alt="" />
-            )}
-          </div>
-          {user?.currentRank}
-          <div className={`${styles.crankArrow}`}>
-            {user?.rankGained ? (
-              <img src="/images/svg/arrow_drop_up.svg" alt="" />
-              ) : (
-              <img src="/images/svg/arrow_drop_down.svg" alt="" />
-            )}
-          </div>
-        </div>
+      <div className={`${styles.leaderboardScrollable}`}>
+        <LeaderboardCard user={user} />
+        <LeaderboardCard user={user} />
+        <LeaderboardCard user={user} />
+        <LeaderboardCard user={user} />
+        <LeaderboardCard user={user} />
+        <LeaderboardCard user={user} />
+        <LeaderboardCard user={user} />
+        <LeaderboardCard user={user} />
+        <LeaderboardCard user={user} />
+        <LeaderboardCard user={user} />
       </div>
     </div>
   );
