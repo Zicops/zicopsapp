4ab084b3758ecacb6ab090a3b5bddac4846ccf8a
Jayanta Sarkar <jtas.sarkar@gmail.com>
2022-09-29 03:27:27 +0530

    lsp added in invite user

--- pages/admin/user/my-users/invite.js
+++ pages/admin/user/my-users/invite.js
@@ -29,7 +29,7 @@ export default function MyUserPage() {
     { name: 'Invite User', component: <InviteUser userType={userType} /> }
   ]);
   const [tab, setTab] = useState(tabData[0].name);
-
+  const [userOrgData, setUserOrgData] = useRecoilState(UsersOrganizationAtom);
   //handle emails
   async function handleMail() {
     if (loading) return;
@@ -43,7 +43,9 @@ export default function MyUserPage() {
 
     let isError = false;
     let errorMsg;
-    const resEmail = await inviteUsers({ variables: { emails: emails } }).catch((err) => {
+    const resEmail = await inviteUsers({
+      variables: { emails: emails, lsp_id: userOrgData?.lsp_id }
+    }).catch((err) => {
       errorMsg = err.message;
       isError = !!err;
     });
