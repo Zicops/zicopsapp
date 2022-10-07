d6e2920c10b65e6f20c4ded737c2f1df51c701a6
VAJRESH <vajresh005@gmail.com>
2022-10-07 23:49:54 +0530

    refactor: multi tab login code commented

--- components/AuthChecker/index.js
+++ components/AuthChecker/index.js
@@ -8,54 +8,54 @@ const AuthChecker = ({ children }) => {
   const [authorized, setAuthorized] = useState(true);
 
   // https://stackoverflow.com/questions/20325763/browser-sessionstorage-share-between-tabs
-  useEffect(() => {
-    // console.log('evess');
-    // transfers sessionStorage from one tab to another
-    var sessionStorage_transfer = function (event) {
-      // console.log('eve');
-      if (!event) event = window.event;
-      if (!event.newValue) return;
-      if (event.key == 'getSessionStorage') {
-        console.log(JSON.stringify(sessionStorage));
-        // another tab asked for the sessionStorage -> send it
-        localStorage.setItem('sessionStorage', JSON.stringify(sessionStorage));
-        // the other tab should now have it, so we're done with it.
-        setTimeout(() => {
-          localStorage.removeItem('sessionStorage'); // <- could do short timeout as well.
-        }, 500);
-      } else if (event.key == 'sessionStorage' && !sessionStorage.length) {
-        // another tab sent data <- get it
-        var data = parseJson(event?.newValue);
+  // useEffect(() => {
+  //   // console.log('evess');
+  //   // transfers sessionStorage from one tab to another
+  //   var sessionStorage_transfer = function (event) {
+  //     // console.log('eve');
+  //     if (!event) event = window.event;
+  //     if (!event.newValue) return;
+  //     if (event.key == 'getSessionStorage') {
+  //       console.log(JSON.stringify(sessionStorage));
+  //       // another tab asked for the sessionStorage -> send it
+  //       localStorage.setItem('sessionStorage', JSON.stringify(sessionStorage));
+  //       // the other tab should now have it, so we're done with it.
+  //       setTimeout(() => {
+  //         localStorage.removeItem('sessionStorage'); // <- could do short timeout as well.
+  //       }, 500);
+  //     } else if (event.key == 'sessionStorage' && !sessionStorage.length) {
+  //       // another tab sent data <- get it
+  //       var data = parseJson(event?.newValue);
 
-        // console.log('data', data);
-        if (data) window.location.reload();
-        // // custom event
-        // const e = new Event('login');
-        // // Dispatch the event.
-        // window.dispatchEvent(e);
+  //       // console.log('data', data);
+  //       if (data) window.location.reload();
+  //       // // custom event
+  //       // const e = new Event('login');
+  //       // // Dispatch the event.
+  //       // window.dispatchEvent(e);
 
-        // authCheck(router.asPath);
-        for (var key in data) {
-          sessionStorage.setItem(key, data[key]);
-        }
-      }
-    };
+  //       // authCheck(router.asPath);
+  //       for (var key in data) {
+  //         sessionStorage.setItem(key, data[key]);
+  //       }
+  //     }
+  //   };
 
-    // listen for changes to localStorage
-    if (window.addEventListener) {
-      window.addEventListener('storage', sessionStorage_transfer, false);
-    } else {
-      window.attachEvent('onstorage', sessionStorage_transfer);
-    }
+  //   // listen for changes to localStorage
+  //   if (window.addEventListener) {
+  //     window.addEventListener('storage', sessionStorage_transfer, false);
+  //   } else {
+  //     window.attachEvent('onstorage', sessionStorage_transfer);
+  //   }
 
-    // Ask other tabs for session storage (this is ONLY to trigger event)
-    if (!sessionStorage.length) {
-      localStorage.setItem('getSessionStorage', 'foobar');
-      localStorage.removeItem('getSessionStorage', 'foobar');
-    }
+  //   // Ask other tabs for session storage (this is ONLY to trigger event)
+  //   if (!sessionStorage.length) {
+  //     localStorage.setItem('getSessionStorage', 'foobar');
+  //     localStorage.removeItem('getSessionStorage', 'foobar');
+  //   }
 
-    return () => window.removeEventListener('storage', sessionStorage_transfer, false);
-  }, []);
+  //   return () => window.removeEventListener('storage', sessionStorage_transfer, false);
+  // }, []);
 
   useEffect(() => {
     // on initial load - run auth check
@@ -79,7 +79,7 @@ const AuthChecker = ({ children }) => {
     // redirect to login page if accessing a private page and not logged in
     const path = url.split('?')[0];
     // console.log(path);
-    if (!!localStorage.getItem('tokenF')) return setAuthorized(true);
+    // if (!!localStorage.getItem('tokenF')) return setAuthorized(true);
 
     const userData = parseJson(sessionStorage.getItem('loggedUser'));
 
