c18fdb91eb7486d87813560e6dbade84f6afdf0e
shawnkit70 <ankitjha844@gmail.com>
2022-10-09 00:43:46 +0530

    added fitler in cohort courses

--- components/UserComps/UserCohorts/ChortMasterTab/CohortMapping/AllCourses/index.js
+++ components/UserComps/UserCohorts/ChortMasterTab/CohortMapping/AllCourses/index.js
@@ -1,16 +1,57 @@
 import CoursesAccHead from '@/components/UserProfile/CoursesAccHead';
 import CardContainer from '@/components/LearnerUserProfile/UserCoursesTab/CardContainer';
+import { useState } from 'react';
 
 const AllCourses = ({ section, handleSubmit = () => {} , isLoading = false}) => {
+  const [filters, setFilters] = useState({ searchQuery: '', cat: '', subCat: '', type: '' });
+  const courseData = section?.data;
   return (
     <>
-      <CoursesAccHead />
+      <CoursesAccHead getFilters={(f) => setFilters(f)}/>
       <CardContainer
         hideTopBar={true}
         isAdmin={true}
         type={section.displayType}
         footerType={section.footerType}
-        courseData={section.data}
+        courseData={courseData?.filter((course) => {
+          const nameFilter = course?.name
+            ?.trim()
+            ?.toLowerCase()
+            ?.includes(filters?.searchQuery?.trim()?.toLowerCase());
+          let langFilter = true,
+            catFilter = true,
+            subCatFilter = true,
+            typeFilter = true;
+
+          // if (filters?.lang)
+          //   langFilter = course?.language
+          //     ?.map((lang) => lang?.toLowerCase()?.trim())
+          //     ?.includes(filters?.lang?.trim()?.toLowerCase());
+          if (filters?.cat)
+            catFilter = course?.category
+              ?.trim()
+              ?.toLowerCase()
+              ?.includes(filters?.cat?.trim()?.toLowerCase());
+          if (filters?.subCat)
+            subCatFilter =
+              course?.sub_categories?.findIndex((subCat) =>
+                subCat?.name
+                  ?.trim()
+                  ?.toLowerCase()
+                  ?.includes(filters?.subCat?.trim()?.toLowerCase())
+              ) > 0 ||
+              course?.sub_category
+                ?.trim()
+                ?.toLowerCase()
+                ?.includes(filters?.subCat?.trim()?.toLowerCase());
+          if (filters?.type)
+            typeFilter = course?.type
+              ?.trim()
+              ?.toLowerCase()
+              ?.includes(filters?.type?.trim()?.toLowerCase());
+
+          return nameFilter && langFilter && catFilter && subCatFilter && typeFilter;
+        })}
         buttonText={'Assign'}
         handleSubmit={handleSubmit}
         isLoading={isLoading}
