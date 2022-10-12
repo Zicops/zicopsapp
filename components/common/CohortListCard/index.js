bbadd248afa04f8a0a646732286024f8f04f92b7
shawnkit70 <ankitjha844@gmail.com>
2022-10-12 21:50:13 +0530

    cohort images show in cohortCardList

--- components/common/CohortListCard/index.js
+++ components/common/CohortListCard/index.js
@@ -3,32 +3,32 @@ import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import styles from './cohortListCard.module.scss';
 
-export default function CohortListCard({ data,isRoundImage = false, children, handleClick=()=>{} , type='cohort' }) {
+export default function CohortListCard({
+  data,
+  isRoundImage = false,
+  children,
+  handleClick = () => {},
+  type = 'cohort'
+}) {
+  const [selectedCohort, setSelectedCohort] = useRecoilState(SelectedCohortDataAtom);
 
-  const [selectedCohort , setSelectedCohort] = useRecoilState(SelectedCohortDataAtom);
+  let imageUrl = '/images/profile-card.png';
 
-  const [imageUrl , setImageUrl] = useState('');
+  if (type === 'cohort') imageUrl = data?.imageUrl;
 
-  useEffect(()=>{
-    if(!selectedCohort?.main) return ;
-
-    // console.log(data,'data')
-    
-    if(type === 'cohort') return setImageUrl(data?.imageUrl);
-    
-    if(type === 'user'){
-      const imageLink = data?.photo_url !=='' ? data?.photo_url : '/images/swagDP.jpg' ;
-     return setImageUrl(imageLink);
-    }
-  },[data])
-
+  if (type === 'user') {
+    const imageLink = data?.photo_url !== '' ? data?.photo_url : '/images/swagDP.jpg';
+    imageUrl = imageLink;
+  }
+  // useEffect(() => {
+  //   if (!selectedCohort?.main) return;
+  // }, [data]);
 
   return (
     <div className={`${styles.listCard}`} onClick={handleClick}>
       {/* course img */}
-      <div className={isRoundImage ? `${styles.imgRoundContainer}`:`${styles.imgContainer}`}>
-        <img src={imageUrl
- || '/images/profile-card.png'} alt="" />
+      <div className={isRoundImage ? `${styles.imgRoundContainer}` : `${styles.imgContainer}`}>
+        <img src={imageUrl || '/images/profile-card.png'} alt="" />
       </div>
 
       <div className={`${styles.cardBody}`}>
@@ -37,7 +37,11 @@ export default function CohortListCard({ data,isRoundImage = false, children, ha
 
         {/* <p className={`${styles.desc}`}>{cohortData?.description}</p> */}
         <p className={`${styles.desc}`}>{data?.description || 'Zicops'}</p>
-        {type === 'user' && <p className={`${styles.designation}`}>{data?.role_in_organization || 'Product Manager'}</p>}
+        {type === 'user' && (
+          <p className={`${styles.designation}`}>
+            {data?.role_in_organization || 'Product Manager'}
+          </p>
+        )}
       </div>
 
       <div className={`${styles.footer}`}>{children}</div>
