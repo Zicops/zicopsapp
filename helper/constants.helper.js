d8e57af30af8b4c1345caa70b4e1814eeca1c906
shawnkit70 <ankitjha844@gmail.com>
2022-10-16 18:09:02 +0530

    update: changed assigned courses

--- helper/constants.helper.js
+++ helper/constants.helper.js
@@ -7,6 +7,11 @@ export const THUMBNAIL_GAP = 25;
 export const LEARNING_FOLDER_CAPACITY = 30;
 export const GIBBERISH_VALUE_FOR_LOGIN_STATE = '32Xnwiqodh98bjkxqw';
 
+
+//need to delete chorot later
+export const ASSIGNED_COURSES = ['cohort','admin','chorot'];
+
+
 export const PUBLIC_PATHS = [
   '/login',
   '/home',
