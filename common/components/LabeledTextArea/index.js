a2f576162eb0c3387a715eecacea970fad06df17
shawnkit70 <ankitjha844@gmail.com>
2022-10-23 13:05:50 +0530

    textArea added

---
+++ common/components/LabeledTextArea/index.js
@@ -0,0 +1,36 @@
+// import { func, bool, string, number, shape } from 'prop-types';
+import { labeledTextareaWrapper } from './orgFormTextArea.module.scss';
+
+export default function LabeledTextarea({ inputOptions, styleClass, changeHandler, isError }) {
+  const {
+    inputName,
+    rows = 4,
+    label,
+    placeholder = ' ',
+    value,
+    maxLength = 300,
+    isRequired,
+    isDisabled,
+    isAutoComplete
+  } = inputOptions;
+  return (
+    <div className={`${labeledTextareaWrapper} ${styleClass}`}>
+      {label && (
+        <label htmlFor={inputName} aria-label={inputName}>
+          {label}
+        </label>
+      )}
+      <textarea
+        name={inputName}
+        placeholder={placeholder}
+        value={value}
+        rows={rows}
+        onChange={changeHandler}
+        maxLength={maxLength}
+        required={!!isRequired}
+        disabled={!!isDisabled}
+        className={isError ? 'error' : ''}
+        autoComplete={isAutoComplete?.toString()}></textarea>
+    </div>
+  );
+}
