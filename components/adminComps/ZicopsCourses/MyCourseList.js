e4435cafaa0f3c33cfb01e29524ed858f90488ab
Jayanta Sarkar <jtas.sarkar@gmail.com>
2022-10-30 04:21:30 +0530

    added search in course page

--- components/adminComps/ZicopsCourses/MyCourseList.js
+++ components/adminComps/ZicopsCourses/MyCourseList.js
@@ -64,7 +64,7 @@ function editCourse(courseId) {
   Router.push(`/admin/courses/${courseId}`);
 }
 
-function MyLatestCourseList({ time }) {
+function MyLatestCourseList({ time, searchParam }) {
   const [pageSize, setPageSize] = useState(6);
   const courseType = useRecoilValue(CourseTypeAtom);
 
@@ -84,7 +84,8 @@ function MyLatestCourseList({ time }) {
       pageSize: 999999,
       pageCursor: '',
       filters: {
-        Type: courseType
+        Type: courseType,
+        SearchText: searchParam
       }
     },
     client: queryClient
@@ -107,21 +108,40 @@ function MyLatestCourseList({ time }) {
     />
   );
 }
+let debounceTimer;
 
 const MyCourseList = () => {
   var time = Date.now();
+  const [searchParam, setSearchParam] = useState('');
+
+  function startSearch(val, delay) {
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(() => {
+      setSearchParam(val);
+    }, delay);
+  }
+
   return (
     <>
       <div className="content-panel">
         <div className="search-area">
           <select className="search" name="search">
             <option value="Name">Name</option>
-            <option value="Owner">Owner</option>
-            <option value="Category">Category</option>
+            <option value="Owner" disabled>
+              Owner
+            </option>
+            <option value="Category" disabled>
+              Category
+            </option>
           </select>
-          <input className="search" type="text" placeholder="Search..." />
+          <input
+            className="search"
+            type="text"
+            placeholder="Search..."
+            onChange={(e) => startSearch(e.target.value, 1000)}
+          />
         </div>
-        <MyLatestCourseList time={time} />
+        <MyLatestCourseList time={time} searchParam={searchParam} />
       </div>
 
       <style jsx>
@@ -132,14 +152,14 @@ const MyCourseList = () => {
             box-shadow: -2px 2px 10px 0 #000000, 2px -2px 5px 0 #686868;
             border-radius: 10px;
           }
-          .search-area{
+          .search-area {
             display: flex;
             justify-content: flex-end;
             align-items: center;
             padding: 0 40px;
             height: 60px;
             gap: 10px;
-          } 
+          }
           .search {
             align: right;
             padding: 5px;
