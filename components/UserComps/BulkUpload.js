1d2d63b97418b9053de4ed2a1e75d5395349ac34
VAJRESH <vajresh005@gmail.com>
2022-11-07 17:22:24 +0530

    feat: download sample bulk inivte sheet added

--- components/UserComps/BulkUpload.js
+++ components/UserComps/BulkUpload.js
@@ -1,9 +1,54 @@
 import UploadForm from '../common/FormComponents/UploadForm';
 
 export default function BulkUpload() {
+  async function CSV_XLSX_File_Selected_Event(e) {
+    let ext = e.target.file;
+    ext = ext.split('.');
+    ext = ext[ext.length - 1];
+    var files = inputElement.files || [];
+    if (!files.length) return;
+    var file = files[0];
+    var reader = new FileReader();
+    reader.onloadend = async function (event) {
+      var arrayBuffer = reader.result;
+      var options = { type: 'array' };
+      var workbook = XLSX.read(arrayBuffer, options);
+      //console.timeEnd();
+
+      var sheetName = workbook.SheetNames;
+      var sheet = workbook.Sheets[sheetName];
+      var sheet_to_html = XLSX.utils.sheet_to_html(sheet);
+      var sheet_to_json = XLSX.utils.sheet_to_json(sheet);
+
+      if (sheet_to_json.length === 0) {
+        var sheet_to_csv = [XLSX.utils.sheet_to_csv(sheet)];
+        var results = sheet_to_csv;
+      }
+
+      if (sheet_to_json.length > 0) {
+        var results = sheet_to_json;
+      }
+
+      let Parsed_File_Obj = {
+        sheet_to_html: sheet_to_html,
+        results: results,
+        ext: ext
+      };
+
+      console.log('Parsed_File_Obj');
+      console.log(Parsed_File_Obj);
+    };
+    reader.readAsArrayBuffer(file);
+  }
   return (
     <>
-      <UploadForm />
+      <UploadForm
+        filePath="/templates/user-invite-template.xlsx"
+        fileName="Bulk Invite Template"
+        handleFileUpload={(e) => {
+          console.log(e.target.files);
+        }}
+      />
     </>
   );
 }
--- components/common/FormComponents/UploadForm/index.js
+++ components/common/FormComponents/UploadForm/index.js
@@ -2,12 +2,23 @@ import { downloadFileFromURI } from '@/helper/utils.helper';
 import BrowseAndUpload from '../BrowseAndUpload';
 import styles from '../formComponents.module.scss';
 
-export default function UploadForm({ customStyles, leftGapClass, filePath }) {
+export default function UploadForm({
+  customStyles,
+  leftGapClass,
+  filePath,
+  fileName = 'Questions Upload Template',
+  handleFileUpload = () => {}
+}) {
   return (
     <div className={styles.uploadForm} style={customStyles}>
       <div className={styles.download}>
         <div className={leftGapClass}></div>
-        <button onClick={() => downloadFileFromURI(filePath, 'Questions Upload Template')}>
+        <button
+          onClick={() => {
+            if (!filePath) return;
+
+            downloadFileFromURI(filePath, fileName);
+          }}>
           Download Template
         </button>
         <p>Sample file format template</p>
@@ -16,7 +27,7 @@ export default function UploadForm({ customStyles, leftGapClass, filePath }) {
       <div className={styles.upload}>
         <label>Upload Profiles: </label>
         <span>Upload Excel filled with data as per the standard template</span>
-        <BrowseAndUpload />
+        <BrowseAndUpload handleFileUpload={handleFileUpload} />
       </div>
     </div>
   );
