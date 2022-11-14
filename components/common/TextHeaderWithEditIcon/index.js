2856b9dfc5de34ff0756319cc036881c30448fee
Jayanta Sarkar <jtas.sarkar@gmail.com>
2022-11-14 18:42:20 +0530

    administrationUI header

---
+++ components/common/TextHeaderWithEditIcon/index.js
@@ -0,0 +1,18 @@
+import styles from './TextHeaderWithEditIcon.module.scss';
+
+export default function TextHeaderWithEditIcon({headingText, showIcon = true, handleClick}) {
+  return (
+    <>
+      <div className={`${styles.textEditIcon}`}>
+        <div className={`${styles.text}`}>{headingText}</div>
+        {showIcon && (
+          <div className={`${styles.Icon}`} onClick={handleClick}>
+            {/* //toggleEditable */}
+            <img src="/images/svg/edit.svg" />
+          </div>
+        )}
+      </div>
+      <hr />
+    </>
+  );
+}
