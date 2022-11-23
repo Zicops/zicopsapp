1009b810573a702245099e245bdb79fcf5e5d944
VAJRESH <vajresh005@gmail.com>
2022-11-23 12:43:12 +0530

    fix: span added in tooltip

--- components/common/ToolTip/index.js
+++ components/common/ToolTip/index.js
@@ -3,14 +3,14 @@ import { Tooltip } from '@mui/material';
 import Fade from '@mui/material/Fade';
 import { makeStyles } from '@material-ui/core';
 
-const ToolTip = ({ children, placement, title = "", image, buttonName }) => {
+const ToolTip = ({ children, placement, title = '', image, buttonName }) => {
   const useTooltipStyles = makeStyles((theme) => ({
     tooltip: {
-      backgroundColor: '#4b5c6b',
+      backgroundColor: '#4b5c6b'
     },
     arrow: {
       '&.MuiTooltip-arrow::before': {
-        background: '#4b5c6b',
+        background: '#4b5c6b'
       }
     }
   }));
@@ -20,35 +20,36 @@ const ToolTip = ({ children, placement, title = "", image, buttonName }) => {
   return (
     <>
       <Tooltip
-        title={title?.length > 0?
-          <>
-            <div
-              style={{
-                fontSize: '13px',
-                fontWeight:'600',
-                padding: '5px',
-                maxWidth: '150px',
-                wordWrap: 'wrap',
-                textAlign: 'center'
-              }}>
-              {image && (
-                <div>
-                </div>
-              )}
-               <div>{title}</div>
-              <div>{buttonName && <button>{buttonName}</button>}</div>
-            </div>
-          </> : ""
+        title={
+          title?.length > 0 ? (
+            <>
+              <div
+                style={{
+                  fontSize: '13px',
+                  fontWeight: '600',
+                  padding: '5px',
+                  maxWidth: '150px',
+                  wordWrap: 'wrap',
+                  textAlign: 'center'
+                }}>
+                {image && <div></div>}
+                <div>{title}</div>
+                <div>{buttonName && <button>{buttonName}</button>}</div>
+              </div>
+            </>
+          ) : (
+            ''
+          )
         }
         placement={placement}
         arrow
         TransitionComponent={Fade}
         TransitionProps={{ timeout: 300 }}
         classes={tooltipClass}
-        enterDelay={1000} 
+        enterDelay={1000}
         // leaveDelay={1000}
       >
-        {children}
+        <span>{children}</span>
       </Tooltip>
     </>
   );
