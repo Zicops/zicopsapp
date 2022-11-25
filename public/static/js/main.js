a365aa8d02aaaf9c291ebf28cf243ffbd48a758c
Jayanta Sarkar <jtas.sarkar@gmail.com>
2022-11-25 18:36:00 +0530

    static about not sticking in chrome

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,15 +1,12 @@
 window.onscroll = function () {
-    if (
-      document.body.scrollTop > 50 ||
-      document.documentElement.scrollTop > 50
-    ) {
-      document.getElementById("navbar").style.height = "50px";
-      document.getElementById("navbar").style.backgroundColor = "#131518";
-    } else {
-        document.getElementById("navbar").style.height = "100px";
-        document.getElementById("navbar").style.backgroundColor = "transparent";
-    }
-}
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById('navbar').style.height = '50px';
+    document.getElementById('navbar').style.backgroundColor = '#131518';
+  } else {
+    document.getElementById('navbar').style.height = '100px';
+    document.getElementById('navbar').style.backgroundColor = 'transparent';
+  }
+};
 
 function reveal() {
   var reveals = document.querySelectorAll('.animate');
@@ -30,3 +27,91 @@ function reveal() {
 
 window.addEventListener('scroll', reveal);
 reveal();
+
+let direction = 'up';
+let prevYPosition = 0;
+
+// const setScrollDirection = () => {
+//   if (scrollRoot.scrollTop > prevYPosition) {
+//     direction = 'down';
+//   } else {
+//     direction = 'up';
+//   }
+
+//   prevYPosition = scrollRoot.scrollTop;
+// };
+
+// $(document).ready(function () {});
+const snapSections = document.querySelectorAll('.snap-section');
+
+let observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // setScrollDirection();
+        console.log(entry, entry.target, direction, prevYPosition);
+        entry.target.style.color = 'red';
+        entry.target.scrollIntoView({behavior: 'smooth'});
+        // entry.target.classList.add('stick-top');
+        // document.getElementById('navbar').style.height = '100px';
+        // document.getElementById('navbar').style.backgroundColor = 'transparent';
+        // alert(entry.target.classList);
+        // observer.unobserve(snapSections);
+      } else {
+        entry.target.style.color = 'blue';
+        // entry.target.classList.remove('stick-top');
+      }
+    });
+  },
+  {
+    // root: null,
+    // rootMargin: '0px 0px',
+    threshold: 0.6
+  }
+);
+
+snapSections.forEach((section) => {
+  observer.observe(section);
+});
+
+// const targets = document.querySelectorAll('[data-observer]');
+// const images = document.querySelectorAll('[data-img]');
+
+// const options = {
+//   rootMargin: '0px',
+//   threshold: 1.0
+// };
+
+// const addClass = (el) => {
+//   if (!el.classList.contains('is-visible')) {
+//     el.classList.add('is-visible');
+//   }
+// };
+
+// const removeClass = (el) => {
+//   if (el.classList.contains('is-visible')) {
+//     el.classList.remove('is-visible');
+//   }
+// };
+
+// const doThings = (entries, observer) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       addClass(entry.target);
+//     } else {
+//       removeClass(entry.target);
+//     }
+//   });
+// };
+
+// const observer = new IntersectionObserver(doThings, options);
+
+// const observer2 = new IntersectionObserver(doThings, { ...options, threshold: 0.4 });
+
+// targets.forEach((target) => {
+//   observer.observe(target);
+// });
+
+// images.forEach((target) => {
+//   observer2.observe(target);
+// });
