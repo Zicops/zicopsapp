4028b54ab5ee462566fd9d7b71276c63e2921e83
Jayanta Sarkar <jtas.sarkar@gmail.com>
2022-12-02 20:09:03 +0530

    Revert "Merge branch 'main' of https://github.com/Zicops/zicopsapp"
    This reverts commit 41a69fa06bbc10e105f25ed71f13716cc98699fa, reversing
    changes made to d0026f5034012e8cc386e0b276dbe4b2642737c7.

--- components/UserProfile/CoursesAccordian/index.js
+++ components/UserProfile/CoursesAccordian/index.js
@@ -250,13 +250,10 @@ const CoursesAccordian = ({ currentUserData = null }) => {
 
   useEffect(async () => {
     const currentTime = new Date().getTime();
-    const _lspId = sessionStorage?.getItem('lsp_id');
-
     const sendData = {
       publish_time: Math.floor(currentTime / 1000),
       pageCursor: '',
-      pageSize: 100,
-      filters: { LspId: _lspId }
+      pageSize: 100
     };
     const res = await loadLastestCourseData({ variables: sendData }).catch((err) => {
       console.log(err);
@@ -307,7 +304,7 @@ const CoursesAccordian = ({ currentUserData = null }) => {
       (courseMap) => courseMap?.user_lsp_id === currentUserData?.userLspId
     );
 
-    if (!userLspCourses?.length) setCourseLoading(false);
+    if(!userLspCourses?.length) setCourseLoading(false);
     const assignedCoursesToUser = userLspCourses;
     if (!assignedCoursesToUser?.length) setCourseLoading(false);
 
