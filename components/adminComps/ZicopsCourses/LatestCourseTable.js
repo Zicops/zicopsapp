137944c5b93d01aa8b22134cb42e2386e22d6b33
VAJRESH <vajresh005@gmail.com>
2022-12-02 13:41:11 +0530

    feat: my course and zicops course updated code structure

---
+++ components/adminComps/ZicopsCourses/LatestCourseTable.js
@@ -0,0 +1,157 @@
+// components\adminComps\ZicopsCourses\LatestCourseTable.js
+
+import { GET_LATEST_COURSES, queryClient } from '@/api/Queries';
+import ZicopsTable from '@/components/common/ZicopsTable';
+import { COURSE_STATUS } from '@/helper/constants.helper';
+import { sortArrByKeyInOrder } from '@/helper/data.helper';
+import { getPageSizeBasedOnScreen, getUnixFromDate } from '@/helper/utils.helper';
+import { CourseTypeAtom } from '@/state/atoms/module.atoms';
+import { UsersOrganizationAtom } from '@/state/atoms/users.atom';
+import { useLazyQuery } from '@apollo/client';
+import { Switch } from '@mui/material';
+import Router from 'next/router';
+import { useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+
+export default function LatestCourseTable({ isEditable = false, zicopsLspId = null }) {
+  const [loadMyCourses, { loading }] = useLazyQuery(GET_LATEST_COURSES, {
+    client: queryClient
+  });
+
+  const courseType = useRecoilValue(CourseTypeAtom);
+  const userOrgData = useRecoilValue(UsersOrganizationAtom);
+
+  const [latestCourses, setLatestCourse] = useState([]);
+  const [courseStatus, setCourseStatus] = useState(COURSE_STATUS.save);
+  const [searchParam, setSearchParam] = useState('');
+
+  useEffect(() => {
+    if (!userOrgData?.lsp_id) return;
+    const time = getUnixFromDate();
+
+    loadMyCourses({
+      variables: {
+        publish_time: time,
+        pageSize: 1000,
+        pageCursor: '',
+        status: courseStatus,
+        filters: {
+          Type: courseType,
+          SearchText: searchParam?.trim(),
+          LspId: zicopsLspId || userOrgData?.lsp_id
+        }
+      }
+    }).then((res) => {
+      const _latestCourses = sortArrByKeyInOrder(
+        res?.data?.latestCourses?.courses?.filter((c) => c?.is_active),
+        'created_at',
+        false
+      );
+      setLatestCourse(_latestCourses);
+    });
+  }, [userOrgData?.lsp_id, searchParam, courseStatus, zicopsLspId]);
+
+  const columns = [
+    {
+      field: 'name',
+      headerName: 'Name',
+      headerClassName: 'course-list-header',
+      flex: 1.5
+    },
+    {
+      field: 'owner',
+      headerClassName: 'course-list-header',
+      headerName: 'Owner',
+      flex: 1
+    },
+    {
+      field: 'category',
+      headerClassName: 'course-list-header',
+      headerName: 'Category',
+      flex: 1
+    },
+    {
+      field: 'expertise_level',
+      headerClassName: 'course-list-header',
+      headerName: 'Level',
+      flex: 1
+    },
+    {
+      field: 'action',
+      headerClassName: 'course-list-header',
+      headerName: 'Action',
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <button
+              style={{
+                cursor: 'pointer',
+                backgroundColor: 'transparent',
+                outline: '0',
+                border: '0'
+              }}
+              onClick={() => Router.push(`/preview?courseId=${params.row.id}`)}>
+              <img src="/images/svg/eye-line.svg" width={20}></img>
+            </button>
+            {isEditable ? (
+              <button
+                style={{
+                  cursor: 'pointer',
+                  backgroundColor: 'transparent',
+                  outline: '0',
+                  border: '0'
+                }}
+                onClick={() => Router.push(`/admin/courses/${params.row.id}`)}>
+                <img src="/images/svg/edit-box-line.svg" width={20}></img>
+              </button>
+            ) : (
+              <button
+                style={{
+                  cursor: 'pointer',
+                  backgroundColor: 'transparent',
+                  outline: '0',
+                  border: '0'
+                }}>
+                <Switch
+                  inputProps={{ 'aria-label': 'Switch demo' }}
+                  defaultChecked
+                  color="success"
+                />
+              </button>
+            )}
+          </>
+        );
+      },
+      flex: 0.5
+    }
+  ];
+
+  return (
+    <>
+      <ZicopsTable
+        columns={columns}
+        data={latestCourses}
+        pageSize={getPageSizeBasedOnScreen()}
+        rowsPerPageOptions={[3]}
+        tableHeight="70vh"
+        loading={loading}
+        showCustomSearch={true}
+        searchProps={{
+          options: [
+            { label: 'Name', value: 'Name' },
+            { label: 'Owner', value: 'Owner' },
+            { label: 'Category', value: 'Category' }
+          ],
+          handleSearch: (val) => setSearchParam(val),
+          filterOptions: [
+            { label: 'Saved', value: COURSE_STATUS.save },
+            { label: 'Published', value: COURSE_STATUS.publish }
+          ],
+          handleFilterOptionChange: setCourseStatus,
+          selectedFilter: courseStatus
+        }}
+      />
+    </>
+  );
+}
--- components/adminComps/ZicopsCourses/MyCourseList.js
+++ components/adminComps/ZicopsCourses/MyCourseList.js
@@ -9,152 +9,153 @@ import { useRecoilValue } from 'recoil';
 import { GET_LATEST_COURSES, queryClient } from '../../../API/Queries';
 import { getUnixFromDate, TableResponsiveRows } from '../../../helper/utils.helper';
 import ZicopsTable from '../../common/ZicopsTable';
+import LatestCourseTable from './LatestCourseTable';
 
-const columns = [
-  {
-    field: 'name',
-    headerName: 'Name',
-    headerClassName: 'course-list-header',
-    flex: 1.5
-  },
-  {
-    field: 'owner',
-    headerClassName: 'course-list-header',
-    headerName: 'Owner',
-    flex: 1
-  },
-  {
-    field: 'category',
-    headerClassName: 'course-list-header',
-    headerName: 'Category',
-    flex: 1
-  },
-  {
-    field: 'expertise_level',
-    headerClassName: 'course-list-header',
-    headerName: 'Level',
-    flex: 1
-  },
-  {
-    field: 'action',
-    headerClassName: 'course-list-header',
-    headerName: 'Action',
-    sortable: false,
-    renderCell: (params) => {
-      return (
-        <>
-          <button
-            style={{ cursor: 'pointer', backgroundColor: 'transparent', outline: '0', border: '0' }}
-            onClick={() => Router.push(`/preview?courseId=${params.row.id}`)}>
-            <img src="/images/svg/eye-line.svg" width={20}></img>
-          </button>
-          <button
-            style={{ cursor: 'pointer', backgroundColor: 'transparent', outline: '0', border: '0' }}
-            onClick={() => editCourse(params.row.id)}>
-            <img src="/images/svg/edit-box-line.svg" width={20}></img>
-          </button>
-        </>
-      );
-    },
-    flex: 0.5
-  }
-];
+// const columns = [
+//   {
+//     field: 'name',
+//     headerName: 'Name',
+//     headerClassName: 'course-list-header',
+//     flex: 1.5
+//   },
+//   {
+//     field: 'owner',
+//     headerClassName: 'course-list-header',
+//     headerName: 'Owner',
+//     flex: 1
+//   },
+//   {
+//     field: 'category',
+//     headerClassName: 'course-list-header',
+//     headerName: 'Category',
+//     flex: 1
+//   },
+//   {
+//     field: 'expertise_level',
+//     headerClassName: 'course-list-header',
+//     headerName: 'Level',
+//     flex: 1
+//   },
+//   {
+//     field: 'action',
+//     headerClassName: 'course-list-header',
+//     headerName: 'Action',
+//     sortable: false,
+//     renderCell: (params) => {
+//       return (
+//         <>
+//           <button
+//             style={{ cursor: 'pointer', backgroundColor: 'transparent', outline: '0', border: '0' }}
+//             onClick={() => Router.push(`/preview?courseId=${params.row.id}`)}>
+//             <img src="/images/svg/eye-line.svg" width={20}></img>
+//           </button>
+//           <button
+//             style={{ cursor: 'pointer', backgroundColor: 'transparent', outline: '0', border: '0' }}
+//             onClick={() => editCourse(params.row.id)}>
+//             <img src="/images/svg/edit-box-line.svg" width={20}></img>
+//           </button>
+//         </>
+//       );
+//     },
+//     flex: 0.5
+//   }
+// ];
 
-function editCourse(courseId) {
-  Router.push(`/admin/courses/${courseId}`);
-}
+// function editCourse(courseId) {
+//   Router.push(`/admin/courses/${courseId}`);
+// }
 
-function MyLatestCourseList({ time }) {
-  const [pageSize, setPageSize] = useState(6);
-  const courseType = useRecoilValue(CourseTypeAtom);
-  const userOrgData = useRecoilValue(UsersOrganizationAtom);
-  const [latestCourses, setLatestCourse] = useState([]);
+// function MyLatestCourseList({ time }) {
+//   const [pageSize, setPageSize] = useState(6);
+//   const courseType = useRecoilValue(CourseTypeAtom);
+//   const userOrgData = useRecoilValue(UsersOrganizationAtom);
+//   const [latestCourses, setLatestCourse] = useState([]);
 
-  const [courseStatus, setCourseStatus] = useState(COURSE_STATUS.save);
-  const [searchParam, setSearchParam] = useState('');
+//   const [courseStatus, setCourseStatus] = useState(COURSE_STATUS.save);
+//   const [searchParam, setSearchParam] = useState('');
 
-  useEffect(() => {
-    const screenWidth = window.screen.width;
-    console.log(screenWidth);
-    TableResponsiveRows.forEach((r, i) => {
-      if (r.breakpoint <= screenWidth) {
-        setPageSize(r.pageSize);
-      }
-    });
-  }, []);
+//   useEffect(() => {
+//     const screenWidth = window.screen.width;
+//     console.log(screenWidth);
+//     TableResponsiveRows.forEach((r, i) => {
+//       if (r.breakpoint <= screenWidth) {
+//         setPageSize(r.pageSize);
+//       }
+//     });
+//   }, []);
 
-  useEffect(() => {
-    if (!userOrgData?.lsp_id) return;
+//   useEffect(() => {
+//     if (!userOrgData?.lsp_id) return;
 
-    loadMyCourses({
-      variables: {
-        publish_time: time,
-        pageSize: 1000,
-        pageCursor: '',
-        status: courseStatus,
-        filters: {
-          Type: courseType,
-          SearchText: searchParam,
-          LspId: userOrgData?.lsp_id
-        }
-      }
-    }).then((res) => {
-      const _latestCourses = sortArrByKeyInOrder(
-        res?.data?.latestCourses?.courses?.filter((c) => c?.is_active),
-        'created_at',
-        false
-      );
-      setLatestCourse(_latestCourses);
-    });
-  }, [userOrgData?.lsp_id, searchParam, courseStatus]);
+//     loadMyCourses({
+//       variables: {
+//         publish_time: time,
+//         pageSize: 1000,
+//         pageCursor: '',
+//         status: courseStatus,
+//         filters: {
+//           Type: courseType,
+//           SearchText: searchParam,
+//           LspId: userOrgData?.lsp_id
+//         }
+//       }
+//     }).then((res) => {
+//       const _latestCourses = sortArrByKeyInOrder(
+//         res?.data?.latestCourses?.courses?.filter((c) => c?.is_active),
+//         'created_at',
+//         false
+//       );
+//       setLatestCourse(_latestCourses);
+//     });
+//   }, [userOrgData?.lsp_id, searchParam, courseStatus]);
 
-  const [loadMyCourses, { loading }] = useLazyQuery(GET_LATEST_COURSES, {
-    client: queryClient
-  });
+//   const [loadMyCourses, { loading }] = useLazyQuery(GET_LATEST_COURSES, {
+//     client: queryClient
+//   });
 
-  // let latestCourses = sortArrByKeyInOrder(
-  //   data?.latestCourses.courses?.filter((c) => c?.is_active),
-  //   'created_at',
-  //   false
-  // );
+//   // let latestCourses = sortArrByKeyInOrder(
+//   //   data?.latestCourses.courses?.filter((c) => c?.is_active),
+//   //   'created_at',
+//   //   false
+//   // );
 
-  return (
-    <ZicopsTable
-      columns={columns}
-      data={latestCourses}
-      pageSize={pageSize}
-      rowsPerPageOptions={[3]}
-      tableHeight="70vh"
-      loading={loading}
-      showCustomSearch={true}
-      searchProps={{
-        options: [
-          { label: 'Name', value: 'Name' },
-          { label: 'Owner', value: 'Owner' },
-          { label: 'Category', value: 'Category' }
-        ],
-        handleSearch: (val) => setSearchParam(val),
-        filterOptions: [
-          { label: 'Saved', value: COURSE_STATUS.save },
-          { label: 'Published', value: COURSE_STATUS.publish }
-        ],
-        handleFilterOptionChange: setCourseStatus,
-        selectedFilter: courseStatus
-      }}
-    />
-  );
-}
-let debounceTimer;
+//   return (
+//     <ZicopsTable
+//       columns={columns}
+//       data={latestCourses}
+//       pageSize={pageSize}
+//       rowsPerPageOptions={[3]}
+//       tableHeight="70vh"
+//       loading={loading}
+//       showCustomSearch={true}
+//       searchProps={{
+//         options: [
+//           { label: 'Name', value: 'Name' },
+//           { label: 'Owner', value: 'Owner' },
+//           { label: 'Category', value: 'Category' }
+//         ],
+//         handleSearch: (val) => setSearchParam(val),
+//         filterOptions: [
+//           { label: 'Saved', value: COURSE_STATUS.save },
+//           { label: 'Published', value: COURSE_STATUS.publish }
+//         ],
+//         handleFilterOptionChange: setCourseStatus,
+//         selectedFilter: courseStatus
+//       }}
+//     />
+//   );
+// }
+// let debounceTimer;
 
 const MyCourseList = () => {
-  var time = getUnixFromDate();
+  // var time = getUnixFromDate();
 
-  function startSearch(val, delay) {
-    clearTimeout(debounceTimer);
-    debounceTimer = setTimeout(() => {
-      setSearchParam(val);
-    }, delay);
-  }
+  // function startSearch(val, delay) {
+  //   clearTimeout(debounceTimer);
+  //   debounceTimer = setTimeout(() => {
+  //     setSearchParam(val);
+  //   }, delay);
+  // }
 
   return (
     <>
@@ -176,7 +177,8 @@ const MyCourseList = () => {
             onChange={(e) => startSearch(e.target.value, 1000)}
           />
         </div> */}
-        <MyLatestCourseList time={time} />
+        {/* <MyLatestCourseList time={time} /> */}
+        <LatestCourseTable isEditable={true} />
       </div>
 
       <style jsx>
