f93fff4ac179c7557578cbcd3e7b817264a25779
shawnkit70 <ankitjha844@gmail.com>
2022-12-04 01:34:07 +0530

    feat: added feature to promote user to admin

--- components/UserComps/MyUser/index.js
+++ components/UserComps/MyUser/index.js
@@ -5,12 +5,12 @@ import LabeledRadioCheckbox from '@/common/FormComponents/LabeledRadioCheckbox';
 import ZicopsTable from '@/common/ZicopsTable';
 import ConfirmPopUp from '@/components/common/ConfirmPopUp';
 import { loadQueryDataAsync } from '@/helper/api.helper';
-import { USER_MAP_STATUS } from '@/helper/constants.helper';
+import { USER_LSP_ROLE, USER_MAP_STATUS } from '@/helper/constants.helper';
 import { sortArrByKeyInOrder } from '@/helper/data.helper';
 import { getUserAboutObject, useUpdateUserAboutData } from '@/helper/hooks.helper';
 import { getPageSizeBasedOnScreen, isWordIncluded } from '@/helper/utils.helper';
 import { ToastMsgAtom } from '@/state/atoms/toast.atom';
-import { DisabledUserAtom } from '@/state/atoms/users.atom';
+import { AdminLearnerListAtom, DisabledUserAtom } from '@/state/atoms/users.atom';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
@@ -22,10 +22,18 @@ export default function MyUser({ getUser, isAdministration = false, customStyle 
   const [disableAlert, setDisableAlert] = useState(false);
   const [isMakeAdminAlert, setIsMakeAdminAlert] = useState(false);
   const [disabledUserList, setDisabledUserList] = useRecoilState(DisabledUserAtom);
+  const [adminLearnerList, setAdminLearnerList] = useRecoilState(AdminLearnerListAtom);
   const [currentDisabledUser, setCurrentDisabledUser] = useState(null);
+  const [currentSelectedUser, setCurrentSelectedUser] = useState(null);
 
-  const { newUserAboutData, setNewUserAboutData, updateAboutUser, updateUserLsp, isFormCompleted, updateUserRole} =
-    useUpdateUserAboutData();
+  const {
+    newUserAboutData,
+    setNewUserAboutData,
+    updateAboutUser,
+    updateUserLsp,
+    isFormCompleted,
+    updateUserRole
+  } = useUpdateUserAboutData();
 
   const [isLoading, setLoading] = useState(true);
   const [filterCol, setFilterCol] = useState('email');
@@ -37,13 +45,13 @@ export default function MyUser({ getUser, isAdministration = false, customStyle 
   useEffect(async () => {
     setLoading(true);
 
-    const usersData = await getUsersForAdmin(true);
-    if (usersData?.error) {
+    const _usersData = await getUsersForAdmin(true);
+    if (_usersData?.error) {
       setLoading(false);
-      return setToastMsg({ type: 'danger', message: `${usersData?.error}` });
+      return setToastMsg({ type: 'danger', message: `${_usersData?.error}` });
     }
-    for (let i = 0; i < usersData.length; i++) {
-      const user = usersData[i];
+    for (let i = 0; i < _usersData.length; i++) {
+      const user = _usersData[i];
       const res = await loadQueryDataAsync(
         GET_USER_LSP_ROLES,
         {
@@ -56,7 +64,10 @@ export default function MyUser({ getUser, isAdministration = false, customStyle 
       user.role = res?.getUserLspRoles?.[0]?.role;
       user.roleData = res?.getUserLspRoles?.[0];
     }
+    //make sure no user without role map is shown
+    const usersData = _usersData?.filter((user) => !!user?.roleData);
     let users = [];
+
     if (isAdministration) {
       users = usersData?.filter((user) => user?.role?.toLowerCase() === 'admin');
       // console.log(users,'users')
@@ -72,6 +83,7 @@ export default function MyUser({ getUser, isAdministration = false, customStyle 
     getUser(selectedUser);
   }, [selectedUser]);
 
+  
   const columns = [
     {
       field: 'email',
@@ -79,7 +91,7 @@ export default function MyUser({ getUser, isAdministration = false, customStyle 
       flex: 2,
       renderHeader: (params) => (
         <div className="center-elements-with-flex">
-          {!isAdministration &&
+          {!isAdministration && (
             <LabeledRadioCheckbox
               type="checkbox"
               isChecked={data?.length !== 0 && selectedUser.length === data.length}
@@ -87,14 +99,14 @@ export default function MyUser({ getUser, isAdministration = false, customStyle 
                 setSelectedUser(e.target.checked ? [...data.map((row) => row)] : []);
               }}
             />
-          }
+          )}
           Email Id
         </div>
       ),
       renderCell: (params) => {
         return (
           <div className="center-elements-with-flex">
-            {!isAdministration &&
+            {!isAdministration && (
               <LabeledRadioCheckbox
                 type="checkbox"
                 isChecked={selectedUser?.find((u) => u?.id === params.id)}
@@ -111,7 +123,7 @@ export default function MyUser({ getUser, isAdministration = false, customStyle 
                   setSelectedUser(userList);
                 }}
               />
-            }
+            )}
             {params.row?.email}
           </div>
         );
@@ -135,8 +147,20 @@ export default function MyUser({ getUser, isAdministration = false, customStyle 
       headerName: 'Role',
       flex: 0.5,
       renderCell: (params) => {
+        let isLearner = false;
+        let isAdmin = false;
+        isAdmin = params?.row?.role?.toLowerCase() !== 'learner';
+        if (adminLearnerList?.admins?.includes(params?.row?.id)) {
+          isLearner = false;
+          isAdmin = true;
+        }
+        if (adminLearnerList?.learners?.includes(params?.row?.id)) {
+          isLearner = true;
+          isAdmin = false;
+        }
+
         return (
-          <span style={{ textTransform: 'capitalize' }}>{params?.row?.role || 'Learner'}</span>
+          <span style={{ textTransform: 'capitalize' }}>{isLearner?'Learner':'Admin'}</span>
         );
       }
     },
@@ -157,17 +181,28 @@ export default function MyUser({ getUser, isAdministration = false, customStyle 
           </span>
         );
       }
-    },
-   
+    }
   ];
   if (!isAdministration) {
-    columns.push(
-       {
+    columns.push({
       field: 'action',
       headerClassName: 'course-list-header',
       headerName: 'Action',
       flex: 0.4,
       renderCell: (params) => {
+        let isLearner = false;
+        let isAdmin = false;
+        isAdmin = params?.row?.role?.toLowerCase() !== 'learner';
+
+        if (adminLearnerList?.admins?.includes(params?.row?.id)) {
+          isLearner = false;
+          isAdmin = true;
+        }
+        if (adminLearnerList?.learners?.includes(params?.row?.id)) {
+          isLearner = true;
+          isAdmin = false;
+        }
+
         const buttonArr = [
           { handleClick: () => router.push(`/admin/user/my-users/${params.id}`) },
           // { handleClick: () => alert(`Edit ${params.id}`) },
@@ -190,22 +225,30 @@ export default function MyUser({ getUser, isAdministration = false, customStyle 
               if (isDisabled) setCurrentDisabledUser(params?.row?.id);
               setDisableAlert(true);
             }
+          },
+          {
+            text: isLearner ? 'Make Admin' : 'Demote Admin',
+            handleClick: () => {
+              setCurrentSelectedUser({
+                ...params?.row,
+                updateTo: isLearner  ? 'Admin' : 'Learner'
+              });
+              setIsMakeAdminAlert(true);
+            }
           }
         ];
 
-        if(params?.row?.role?.toLowerCase() === 'learner'){
-          buttonArr.push({text:'Make Admin', handleClick:()=>{setIsMakeAdminAlert(true),updateUserRole(params?.row)}})
-        }
-       return (
-        <>
-          <EllipsisMenu
-            buttonArr={buttonArr}
-          />
-        </>
-      )}
-    }
-    )
-  } 
+        // if(params?.row?.role?.toLowerCase() === 'learner'){
+        //   buttonArr.push({text:'Demote Admin', handleClick:()=>{setIsMakeAdminAlert(true),updateUserRole(params?.row)}})
+        // }
+        return (
+          <>
+            <EllipsisMenu buttonArr={buttonArr} />
+          </>
+        );
+      }
+    });
+  }
   const options = [
     { label: 'Email', value: 'email' },
     { label: 'First Name', value: 'first_name' },
@@ -277,10 +320,82 @@ export default function MyUser({ getUser, isAdministration = false, customStyle 
           }}
         />
       )}
-      {isMakeAdminAlert && <ConfirmPopUp title={`Are you sure you want to make ${newUserAboutData?.email} an Admin`} btnObj={{
-        handleClickLeft: async()=>{},
-        handleClickRight: () => setIsMakeAdminAlert(false)
-      }} />}
+      {isMakeAdminAlert && (
+        <ConfirmPopUp
+          title={`Are you sure you want to make ${currentSelectedUser?.email} to ${currentSelectedUser?.updateTo}`}
+          btnObj={{
+            handleClickLeft: async () => {
+              console.log(currentSelectedUser);
+              const isRoleUpdate = await updateUserRole(currentSelectedUser);
+              console.log(isRoleUpdate, 'role update');
+              if (!isRoleUpdate)
+                return setToastMsg({
+                  type: 'danger',
+                  message: `Error while changing role of ${currentSelectedUser?.email}`
+                });
+              console.log(isRoleUpdate);
+              // we will check updateTo and that key
+              // first check if it is in the learner or admin
+              // if updated to is admin check learner
+              if (currentSelectedUser?.updateTo?.toLowerCase() === USER_LSP_ROLE?.admin) {
+                let foundAt = -1;
+                //checking if user is in learner atom or not, if he is in this then remove him from this and add them to admin
+                const _updatedLearners = adminLearnerList?.learners?.filter((userId, index) => {
+                  if (userId === isRoleUpdate?.user_id) {
+                    foundAt = index;
+                    return true;
+                  }
+                  return false;
+                });
+                if (foundAt >= 0) {
+                  const learners = structuredClone(adminLearnerList?.learners);
+                  learners?.splice(foundAt, 1);
+                  console.log('1',learners);
+                  setAdminLearnerList((prevValue) => ({
+                    admins: [...prevValue?.admins, isRoleUpdate?.user_id],
+                    learners: [...learners]
+                  }));
+                } else {
+                  console.log('2');
+                  setAdminLearnerList((prevValue) => ({
+                    ...prevValue,
+                    admins: [...prevValue?.admins, isRoleUpdate?.user_id]
+                  }));
+                }
+              }
+              // same thing for learner also
+              if (currentSelectedUser?.updateTo?.toLowerCase() === USER_LSP_ROLE?.learner) {
+                let foundAt = -1;
+                const adminUsers = adminLearnerList?.admins?.filter((userId, index) => {
+                  if (userId === isRoleUpdate?.user_id) {
+                    foundAt = index;
+                    return true;
+                  }
+                  return false;
+                });
+                if (foundAt >= 0) {
+                  const admins = structuredClone(adminLearnerList?.admins);
+                  admins?.splice(foundAt, 1);
+                  console.log(admins,'3');
+                  setAdminLearnerList((prev) => ({
+                    admins: [...admins],
+                    learners: [...prev?.learners, isRoleUpdate?.user_id]
+                  }));
+                } else {
+                  console.log('4');
+                  setAdminLearnerList((prev) => ({
+                    ...prev,
+                    learners: [...prev?.learners, isRoleUpdate?.user_id]
+                  }));
+                }
+              }
+              setCurrentSelectedUser(null);
+              return setIsMakeAdminAlert(false);
+            },
+            handleClickRight: () => setIsMakeAdminAlert(false)
+          }}
+        />
+      )}
     </>
   );
 }
