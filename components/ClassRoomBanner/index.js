59c6b6792132e22e861a3f6b604c5d8e602ca087
sonali kokalki <sonalikokalki@gmail.com>
2022-12-17 15:28:08 +0530

    changed the UI of Classroom page slider content.

--- components/ClassRoomBanner/index.js
+++ components/ClassRoomBanner/index.js
@@ -15,40 +15,28 @@ const ClassRoomBanner = ({ data }) => {
         <div className={`${styles.classroomLogo}`}>
           <img src={`${data.logo}`} alt="" />
         </div>
-        <div className={`${styles.classroomTitle}`}>{data.title}</div>
-        <div className={`${styles.classroomBy}`}>
-          By <span>{data.by}</span>
+        <div className={`${styles.classroomTitle}`}>
+       {data.title}</div>
+        <div className={`${styles.classroomByDesisgnation}`}>
+          <div className={`${styles.level}`}>Level: <span>{data.level}</span></div>
+          <div className={`${styles.level}`}>Duration: <span>{data.duration}</span></div>
         </div>
-        <div className={`${styles.classroomByDesisgnation}`}>{data.designation}</div>
-        <div className={`${styles.classroomPrice}`}>
-          <div className={`${styles.classroomIcon}`}>
-            <img src="/images/svg/sell.svg" alt="" />
-          </div>
-          Rs. {data.price}/Seat
+        <div className={`${styles.bannerCat}`}>
+          <ul>
+            <li>{data.cat[0]}</li>
+            <li>{data.cat[1]}</li>
+            <li>{data.cat[2]}</li>
+          </ul>
         </div>
         <div className={`${styles.classroomBannerButtons}`}>
           <ToolTip title={LEARNER_CLASSROOM.bookNow}>
-            <button className={`${styles.bookNowButton}`}>Book Now</button>
+            <button className={`${styles.bookNowButton}`}>Add to my Learning Folder</button>
           </ToolTip>
           <ToolTip title={LEARNER_CLASSROOM.seeMore}>
             <button onClick={() => router.push(`${data?.link}?data=${passData}`, `${data?.link}`)}>
-              See More
+            See Course
             </button>
           </ToolTip>
-        </div>
-        <div className={`${styles.classroomSchedule}`}>
-          <div className={`${styles.classroomScheduleDate}`}>
-            <div className={`${styles.classroomIcon}`}>
-              <img src="/images/svg/event.svg" alt="" />
-            </div>
-            {data.date}
-          </div>
-          <div className={`${styles.classroomScheduleTime}`}>
-            <div className={`${styles.classroomIcon}`}>
-              <img src="/images/svg/schedule.svg" alt="" />
-            </div>
-            {data.time}
-          </div>
         </div>
       </div>
     </div>
