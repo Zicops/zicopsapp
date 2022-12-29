f99302a1f38c9888dff3ed5d81bb30c41af99799
VAJRESH <vajresh005@gmail.com>
2022-12-29 08:34:41 +0530

    feat: video player started

--- helper/constants.helper.js
+++ helper/constants.helper.js
@@ -51,6 +51,7 @@ export const HIDE_HEADER_FOOTER_FOR_ROUTE = [
   '/create-learning-space/org-unit-form',
   '/homepage',
   '/static',
+  '/video-player',
   '/learning-spaces'
 ];
 
@@ -76,7 +77,7 @@ export const TOOLTIP_IMG_SRC = '/images/svg/error_outline_dark.svg';
 export const VIDEO_FILE_TYPES = '.mp4, .mkv';
 export const IMAGE_FILE_TYPES = '.jpg, .jpeg, .png, .svg, .gif';
 export const MIN_COURSE_LENGTH = 25;
-export const PROFILE_IMAGE_TYPE = '.jpg, .jpeg, .png, .svg'
+export const PROFILE_IMAGE_TYPE = '.jpg, .jpeg, .png, .svg';
 
 export const DEFAULT_VALUES = {
   image: 'default-image.jpg',
@@ -108,22 +109,21 @@ export const COURSE_TOPIC_STATUS = {
 //need to delete later
 export const PRODUCT_TOUR_PATHS = ['exams'];
 
-
 export const NOTIFICATION_TITLES = {
   cohortAssign: 'New Cohort Mapped',
   cohortUnassign: 'Cohort Unmapped',
   courseAssign: 'New Course Assigned',
   courseUnssigned: 'Course Unassigned'
-}
+};
 
 export const COURSE_SELF_ASSIGN_LIMIT = 30;
 
 export const EMAIL_TEMPLATE_IDS = {
-  courseAssignMandatory:'d-bf691d7c93794afca36c326cd032ccbf',
-  courseAssignNotMandatory:'d-2f834ed850ba43aab5e55e622f0725c1',
-  courseUnassign:'d-1d43080c8386430ca24d5050130d5c69',
-  cohortAssign:'d-1c44165b36034b839d9b7e7a1035f19b',
-  cohortUnassign:'d-3801f540f2a948729db6019981ace2c2',
-  cohortManagerAssign:'d-c9382610d3bb46f28291bca9f5d97cd4',
-  cohortManagerUnassign:'d-8b97231c38a44b6facb05172f39cc714'
-}
+  courseAssignMandatory: 'd-bf691d7c93794afca36c326cd032ccbf',
+  courseAssignNotMandatory: 'd-2f834ed850ba43aab5e55e622f0725c1',
+  courseUnassign: 'd-1d43080c8386430ca24d5050130d5c69',
+  cohortAssign: 'd-1c44165b36034b839d9b7e7a1035f19b',
+  cohortUnassign: 'd-3801f540f2a948729db6019981ace2c2',
+  cohortManagerAssign: 'd-c9382610d3bb46f28291bca9f5d97cd4',
+  cohortManagerUnassign: 'd-8b97231c38a44b6facb05172f39cc714'
+};
---
+++ pages/video-player.js
@@ -0,0 +1,9 @@
+import VideoPlayer from 'lib/VideoPlayer';
+
+export default function VideoPlayerPage() {
+  return (
+    <>
+      <VideoPlayer videoSrc="/videos/zicops-product-demo-learner-panel.mp4" />
+    </>
+  );
+}
