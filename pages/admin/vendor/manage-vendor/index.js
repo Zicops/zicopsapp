3265bdc1c7a5f121755c4d032617f9f5763e64bb
ANUPAM-RO <68887502+ANUPAM-RO@users.noreply.github.com>
2023-01-20 19:33:33 +0530

    vendor table added

--- pages/admin/vendor/manage-vendor/index.js
+++ pages/admin/vendor/manage-vendor/index.js
@@ -1,14 +1,19 @@
+import AdminHeader from '@/components/common/AdminHeader';
+import MainBody from '@/components/common/MainBody';
 import Sidebar from '@/components/common/Sidebar';
 import { vendorSideBarData } from '@/components/common/Sidebar/Logic/sidebar.helper';
+
+
+import MyVendor from '@/components/VendorComps/MyVendor';
 
 export default function ManageVendor() {
   return (
     <>
       <Sidebar sidebarItemsArr={vendorSideBarData} />
-
-      <div>
-        <p>Manage Vendor</p>
-      </div>
+      <MainBody>
+        <AdminHeader title="Vendor List" isAddShown={true} />
+        <MyVendor />
+      </MainBody>
     </>
   );
 }
