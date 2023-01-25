35dbb498f02bdb4406a11165bd995f30d067eb98
ANUPAM-RO <68887502+ANUPAM-RO@users.noreply.github.com>
2023-01-25 18:36:51 +0530

    images added

---
+++ pages/admin/vendor/market-yard/[vendorid]/index.js
@@ -0,0 +1,50 @@
+import Sidebar from '@/components/common/Sidebar';
+import { vendorSideBarData } from '@/components/common/Sidebar/Logic/sidebar.helper';
+import ZicopsCarousel from '@/components/ZicopsCarousel';
+import { myVendors, coursesVendor , vendorProfiles } from '@/components/VendorComps/Logic/vendorComps.helper.js';
+import TabContainer from '@/common/TabContainer';
+import { useState } from 'react';
+import AboutVendor from '@/components/VendorComps/AboutVendor';
+import { useRouter } from 'next/router';
+import CoursesVendor from '@/components/VendorComps/CoursesVendor';
+import ProfileVendor from '@/components/VendorComps/ProfileVendor';
+
+export default function VendorInfo() {
+  const router = useRouter();
+  const vendorId = router.query.vendorId || '0'; //Change the 1 to null
+    console.log("vendorId", vendorId);
+   const vendorProfileData =  vendorProfiles?.filter((data)=> data?.vendorId === vendorId)
+  const tabData = [
+    {
+      name: 'About',
+      component: <AboutVendor data={myVendors[vendorId]} />
+    },
+    {
+      name: 'Courses',
+      component: <CoursesVendor coursesData={coursesVendor[vendorId]} />
+    },
+    {
+      name: 'Profile',
+        component: <ProfileVendor profileData={vendorProfileData} />
+    }
+  ];
+
+  const [tab, setTab] = useState(tabData[0].name);
+
+  return (
+    <>
+      <Sidebar sidebarItemsArr={vendorSideBarData} />
+      <div style={{ height: '30vh', marginTop: '70px' }}>Frame in Progress</div>
+
+      <TabContainer
+        tabData={tabData}
+        tab={tab}
+        setTab={setTab}
+        footerObj={{
+          showFooter: false
+        }}
+        customStyles={{ height: '100%' }}
+      />
+    </>
+  );
+}
