c7a079fded256b946ed989902b3d200e777c461f
Ayush Jhunjhunwala <ayushjhun13@gmail.com>
2023-01-25 15:49:29 +0530

    router push for manage vendor to add vendor

--- pages/admin/vendor/manage-vendor/index.js
+++ pages/admin/vendor/manage-vendor/index.js
@@ -7,36 +7,70 @@ import { vendorSideBarData } from '@/components/common/Sidebar/Logic/sidebar.hel
 import MyVendor from '@/components/VendorComps/MyVendor';
 import VendorPopUp from '@/components/VendorComps/VendorPopUp';
 import VendorAdd from '@/components/VendorComps/AddVendor';
+import { useRouter } from 'next/router';
 
 export default function ManageVendor() {
   const [isOpen, setIsOpen] = useState(false);
-  const [isVendorType, setIsVendorType] = useState("");
-  const [isVendorLevel, setIsVendorLevel] = useState("");
+  const [isVendorType, setIsVendorType] = useState('');
+  const [isVendorLevel, setIsVendorLevel] = useState('');
 
   const onPlusHandler = () => {
-    console.log("hello");
+    console.log('hello');
     setIsOpen(true);
-  }
+  };
 
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/admin/vendor/manage-vendor/addVendor');
+  };
 
   return (
     <>
       <Sidebar sidebarItemsArr={vendorSideBarData} />
       <MainBody>
-        <AdminHeader title="Vendor List"
+        <AdminHeader
+          title="Vendor List"
           isAddShown={true}
           handleClickForPlus={onPlusHandler}
           isProductTooltip={false}
-          />
+        />
         <MainBodyBox>
           <MyVendor />
-          <VendorPopUp open={isOpen} title="Create Vendor" popUpState={[isOpen, setIsOpen]} 
+          <VendorPopUp
+            open={isOpen}
+            title="Create Vendor"
+            popUpState={[isOpen, setIsOpen]}
             size="small"
-            closeBtn = {{name: "Cancel"}}
-            submitBtn = {{name: "Submit"}}
-          isFooterVisible={true}>
-            <VendorAdd title="Vendor type?" label1="Company" lable2="Individual/Freelancer" value1="company" value2="individual" name1="vendorType" changeHandler1={(e) => setIsVendorType(e.target.value)} changeHandler2={(e) => setIsVendorType(e.target.value)} vendor={isVendorType} checked1={isVendorType == "company"} checked2={isVendorType == "individual"} />
-           <VendorAdd title ="At what level do you want to add the vendor?" label1="Organization" lable2="Learning space Level" value1="organization" value2="lsp" name1="vendorLevel" changeHandler1 ={(e)=>setIsVendorLevel(e.target.value)}  changeHandler2 ={(e)=>setIsVendorLevel(e.target.value)} vendor={isVendorLevel} checked1={isVendorType == "organization"} checked2={isVendorType == "lsp"} />
+            closeBtn={{ name: 'Cancel' }}
+            submitBtn={{ name: 'Submit', handleClick: handleClick }}
+            isFooterVisible={true}>
+            <VendorAdd
+              title="Vendor type?"
+              label1="Company"
+              lable2="Individual/Freelancer"
+              value1="company"
+              value2="individual"
+              name1="vendorType"
+              changeHandler1={(e) => setIsVendorType(e.target.value)}
+              changeHandler2={(e) => setIsVendorType(e.target.value)}
+              vendor={isVendorType}
+              checked1={isVendorType == 'company'}
+              checked2={isVendorType == 'individual'}
+            />
+            <VendorAdd
+              title="At what level do you want to add the vendor?"
+              label1="Organization"
+              lable2="Learning space Level"
+              value1="organization"
+              value2="lsp"
+              name1="vendorLevel"
+              changeHandler1={(e) => setIsVendorLevel(e.target.value)}
+              changeHandler2={(e) => setIsVendorLevel(e.target.value)}
+              vendor={isVendorLevel}
+              checked1={isVendorType == 'organization'}
+              checked2={isVendorType == 'lsp'}
+            />
           </VendorPopUp>
         </MainBodyBox>
       </MainBody>
