ca8cae4ab63cd2355c2ea0fa1935e2d02d50a314
VAJRESH <vajresh005@gmail.com>
2023-02-09 00:27:57 +0530

    feat: course consumption stats table data add

--- components/AdminAnalyticsDashboardComp/CourseConsumptionStatistics/index.js
+++ components/AdminAnalyticsDashboardComp/CourseConsumptionStatistics/index.js
@@ -1,39 +1,105 @@
-import DashboardTable from './DashboardTable';
-import styles from '../adminAnalyticsDashboard.module.scss';
+import Button from '@/components/common/Button';
+import ZicopsTable from '@/components/common/ZicopsTable';
 import Dropdown from '@/components/DashboardComponents/Dropdown';
-import CardHeader from '@/components/DashboardComponents/CardHeader';
 import { useHandleCatSubCat } from '@/helper/hooks.helper';
 import { useState } from 'react';
+import styles from '../adminAnalyticsDashboard.module.scss';
+import useHandleCourseConsumption from '../Logic/useHandleCourseConsumption';
 
 export default function CourseConsumptionStatistics() {
   const { catSubCat, setActiveCatId } = useHandleCatSubCat();
-  const [filters, setFilters] = useState({
-    category: null,
-    subCategory: null
-  });
-  const optionsCat = [{ value: '', label: '-- Select --' }, ...catSubCat?.cat];
-  const catValue = { value: filters.category, label: filters.category };
-  const optionsSubCat = [{ value: '', label: '-- Select --' }, ...catSubCat?.subCat];
-  const subCatValue = { value: filters.subCategory, label: filters.subCategory };
-  const download = {
-    text: 'Download',
-    image: '/images/svg/downloadBlue.svg'
-  };
-  const search = {
-    text: '',
-    image: '/images/svg/searchBlue.svg'
-  };
+  const { tableData, filters, setFilters } = useHandleCourseConsumption();
+
+  const columns = [
+    {
+      field: 'courseName',
+      headerName: 'Course name',
+      headerClassName: 'course-list-header',
+      width: 300
+    },
+    {
+      field: 'activeLearners',
+      headerName: 'Active learners',
+      type: 'number',
+      headerClassName: 'course-list-header',
+      headerAlign: 'center',
+      align: 'center',
+      width: 180
+    },
+    {
+      field: 'completedBy',
+      headerName: 'Completed by',
+      type: 'number',
+      headerAlign: 'center',
+      align: 'center',
+      headerClassName: 'course-list-header',
+      width: 180
+    },
+    {
+      field: 'averageCompletionTime',
+      headerName: 'Average completion time',
+      type: 'number',
+      headerAlign: 'center',
+      align: 'center',
+      headerClassName: 'course-list-header',
+      width: 250
+    },
+    {
+      field: 'averageCompliance',
+      headerName: 'Average compliance %',
+      type: 'number',
+      headerAlign: 'center',
+      align: 'center',
+      headerClassName: 'course-list-header',
+      width: 250
+    },
+    {
+      field: 'publishedOn',
+      headerName: 'Published on',
+      type: 'number',
+      headerAlign: 'center',
+      align: 'center',
+      headerClassName: 'course-list-header',
+      width: 180
+    },
+    {
+      field: 'ownedBy',
+      headerName: 'Owned by',
+      headerAlign: 'center',
+      align: 'center',
+      headerClassName: 'course-list-header',
+      width: 130
+    },
+    {
+      field: 'duration',
+      headerName: 'Duration',
+      type: 'number',
+      headerAlign: 'center',
+      align: 'center',
+      headerClassName: 'course-list-header',
+      width: 100
+    }
+  ];
+
   return (
     <div className={`${styles.wrapper}`}>
       <div className={`${styles.wrapperHeading}`}>
         Course consumption statistics
         <div style={{ display: 'flex', gap: 10, justifyContent: 'center' }}>
-          <CardHeader props={search} />
-          <CardHeader props={download} />
+          <Button styleClass={styles.downloadBtn} text={<img src="/images/svg/searchBlue.svg" />} />
+          <Button
+            styleClass={styles.downloadBtn}
+            text={
+              <>
+                <img src="/images/svg/downloadBlue.svg" />
+                <p>Download</p>
+              </>
+            }
+          />
           <Dropdown
             placeholder={'Category'}
-            options={optionsCat}
-            value={catValue}
+            options={[{ value: '', label: '-- Select --' }, ...catSubCat?.cat]}
+            value={{ value: filters.category, label: filters.category }}
             changeHandler={(e) => {
               setActiveCatId(e);
               setFilters({ ...filters, category: e.value, subCategory: null });
@@ -41,19 +107,24 @@ export default function CourseConsumptionStatistics() {
           />
         </div>
       </div>
+
       <div className={`${styles.wrapperSubHeading}`}>
         All category courses
         <Dropdown
           placeholder={'Sub-category'}
-          options={optionsSubCat}
-          value={subCatValue}
-          changeHandler={(e) => {
-            setFilters({ ...filters, subCategory: e.value });
-          }}
+          options={[{ value: '', label: '-- Select --' }, ...catSubCat?.subCat]}
+          value={{ value: filters.subCategory, label: filters.subCategory }}
+          changeHandler={(e) => setFilters({ ...filters, subCategory: e.value })}
         />
       </div>
 
-      <DashboardTable />
+      <ZicopsTable
+        columns={columns}
+        data={tableData}
+        pageSize={5}
+        rowsPerPageOptions={[3]}
+        tableHeight="55vh"
+      />
     </div>
   );
 }
