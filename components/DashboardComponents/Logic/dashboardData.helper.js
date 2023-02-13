0f3301fb88adc10684149263e2ee49b08312f07d
VAJRESH <vajresh005@gmail.com>
2023-02-13 16:46:20 +0530

    feat: code refactored for dashboard components

--- components/DashboardComponents/Logic/dashboardData.helper.js
+++ components/DashboardComponents/Logic/dashboardData.helper.js
@@ -1,177 +1,180 @@
-export const levelCount = ['40','100','200'];
-export const difficultyLevel = ['Beginner','Competent','Proficient'];
+export const levelCount = ['40', '100', '200'];
+export const difficultyLevel = ['Beginner', 'Competent', 'Proficient'];
 
 export const data = [
-    {
-        id:'1',
-        levelCount:'1000',
-        difficultyLevel:'Beginner'
-    },
-    {
-        id:'2',
-        levelCount:'100',
-        difficultyLevel:'Competent'
-    },
-    {
-        id:'3',
-        levelCount:'10',
-        difficultyLevel:'Proficient'
-    }
-] 
+  {
+    id: '1',
+    levelCount: '1000',
+    difficultyLevel: 'Beginner'
+  },
+  {
+    id: '2',
+    levelCount: '100',
+    difficultyLevel: 'Competent'
+  },
+  {
+    id: '3',
+    levelCount: '10',
+    difficultyLevel: 'Proficient'
+  }
+];
 export const cardData = [
-    {   
-        id:1,
-        cardTitle:'Categories',
-        cardImage:'/images/svg/categories.svg',
-        cardCount:13,
-        cardText:'Categories'
-    },
-    {   id:2,
-        cardTitle:'Sub-categories',
-        cardImage:'/images/svg/workspaces.svg',
-        cardCount:36,
-        cardText:'Sub-categories'
-    },
-    {
-        id:3,
-        cardTitle:'My Courses',
-        cardImage:'/images/svg/local_library.svg',
-        cardCount:38,
-        cardText:'18 assigned courses'
-    },
-    {   
-        id:4,
-        cardTitle:'Zicops Courses',
-        cardImage:'/images/svg/Logo.svg',
-        cardCount:56,
-        cardText:'32 assigned courses'
-    }
-]
+  {
+    id: 1,
+    cardTitle: 'Categories',
+    cardImage: '/images/svg/categories.svg',
+    cardCount: 13,
+    cardText: 'Categories'
+  },
+  {
+    id: 2,
+    cardTitle: 'Sub-categories',
+    cardImage: '/images/svg/workspaces.svg',
+    cardCount: 36,
+    cardText: 'Sub-categories'
+  },
+  {
+    id: 3,
+    cardTitle: 'My Courses',
+    cardImage: '/images/svg/local_library.svg',
+    cardCount: 38,
+    cardText: '18 assigned courses'
+  },
+  {
+    id: 4,
+    cardTitle: 'Zicops Courses',
+    cardImage: '/images/svg/Logo.svg',
+    cardCount: 56,
+    cardText: '32 assigned courses'
+  }
+];
 export const myCoursesAvailabilityData = [
-  {   
-    id:1,
-    cardTitle:'Published',
-    cardImage:'/images/svg/publish.svg',
-    cardCount:23,
-    cardText:'Published courses'
-},
-{   id:2,
-    cardTitle:'Ready for publishing',
-    cardImage:'/images/svg/done.svg',
-    cardCount:38,
-    cardText:'Ready to be published'
-},
-{
-    id:3,
-    cardTitle:'Saved',
-    cardImage:'/images/svg/save.svg',
-    cardCount:58,
-    cardText:'Saved courses'
-},
-{   
-    id:4,
-    cardTitle:'Expired',
-    cardImage:'/images/svg/timer.svg',
-    cardCount:43,
-    cardText:'Expired courses'
-}
-]
-export const badgeData =
-    {  
-        badgeCount:15,
-        badgeText:'Courses'
-    }
+  {
+    id: 1,
+    cardTitle: 'Published',
+    cardImage: '/images/svg/publish.svg',
+    cardCount: 23,
+    cardText: 'Published courses'
+  },
+  {
+    id: 2,
+    cardTitle: 'Ready for publishing',
+    cardImage: '/images/svg/done.svg',
+    cardCount: 38,
+    cardText: 'Ready to be published'
+  },
+  {
+    id: 3,
+    cardTitle: 'Saved',
+    cardImage: '/images/svg/save.svg',
+    cardCount: 58,
+    cardText: 'Saved courses'
+  },
+  {
+    id: 4,
+    cardTitle: 'Expired',
+    cardImage: '/images/svg/timer.svg',
+    cardCount: 43,
+    cardText: 'Expired courses'
+  }
+];
+export const badgeData = {
+  badgeCount: 15,
+  badgeText: 'Courses'
+};
 export const cardHeaderData = {
-        image :'/images/svg/lan.svg',
-        text :'Site Map'
-    }
+  image: '/images/svg/lan.svg',
+  text: 'Site Map'
+};
 export const availabilityCardData = [
   {
-    language : 'Hindi',
+    language: 'Hindi',
     Progress: 32
   },
   {
-    language : 'English',
+    language: 'English',
     Progress: 68
-  }]
-export const statisticsCardData = [{
-    id:1,
-    cardTitle : 'Most added/assigned',
-    cardImage: '/images/svg/trending_up.svg',
-    cardText :'Java Beginners for Enthusiasts'
+  }
+];
+export const statisticsCardData = [
+  {
+    id: 1,
+    title: 'Most added/assigned',
+    icon: '/images/svg/trending_up.svg',
+    courseName: 'Java Beginners for Enthusiasts',
+    learnerCount: 45
   },
   {
-    id:2,
-    cardTitle : 'Least added/assigned',
-    cardImage: '/images/svg/trending_down.svg',
-    cardText :'Complete Java Programming-Beginner to Advance'
-  }]
+    id: 2,
+    title: 'Least added/assigned',
+    icon: '/images/svg/trending_down.svg',
+    courseName: 'Complete Java Programming-Beginner to Advance',
+    learnerCount: 55
+  }
+];
 export const CategoryConsumptionData = [
-    {
-      id: 1,
-      skill: 'UI/UX Design',
-      complete: 5,
-      zicopsCourse:3,
-      days: '01',
-      time1: 30
-    },
-    {
-      id: 2,
-      skill: 'Project Management',
-      complete: 8,
-      zicopsCourse:3,
-      days: '03',
-      time1: 60
-    },
-    {
-      id: 3,
-      skill: 'JAVA fundamentls',
-      complete: 6,
-      zicopsCourse:3,
-      days: '06',
-      time1: 50
-    },
-    {
-      id: 4,
-      skill: 'Product Design',
-      complete: 2,
-      zicopsCourse:3,
-      days: '09',
-      time1:75
-    },
-    {
-      id: 5,
-      skill: 'Bussiness Management',
-      complete: 5,
-      zicopsCourse:3,
-      days: '12',
-      time1: 20
-    },
-    {
-      id: 6,
-      skill: 'Finance',
-      complete: 7,
-      zicopsCourse:3,
-      days: '15',
-      time1: 45
-    },
-    {
-      id: 7,
-      skill: 'UI Developer',
-      complete: 8,
-      zicopsCourse:3,
-      days: '18',
-      time1: 50
-    },
-    {
-      id: 8,
-      skill: 'Animation',
-      complete: 3.5,
-      zicopsCourse:3,
-      days: '21',
-      time1: 60
-    }
-  ];
-  
-
-  
+  {
+    id: 1,
+    skill: 'UI/UX Design',
+    complete: 5,
+    zicopsCourse: 3,
+    days: '01',
+    time1: 30
+  },
+  {
+    id: 2,
+    skill: 'Project Management',
+    complete: 8,
+    zicopsCourse: 3,
+    days: '03',
+    time1: 60
+  },
+  {
+    id: 3,
+    skill: 'JAVA fundamentls',
+    complete: 6,
+    zicopsCourse: 3,
+    days: '06',
+    time1: 50
+  },
+  {
+    id: 4,
+    skill: 'Product Design',
+    complete: 2,
+    zicopsCourse: 3,
+    days: '09',
+    time1: 75
+  },
+  {
+    id: 5,
+    skill: 'Bussiness Management',
+    complete: 5,
+    zicopsCourse: 3,
+    days: '12',
+    time1: 20
+  },
+  {
+    id: 6,
+    skill: 'Finance',
+    complete: 7,
+    zicopsCourse: 3,
+    days: '15',
+    time1: 45
+  },
+  {
+    id: 7,
+    skill: 'UI Developer',
+    complete: 8,
+    zicopsCourse: 3,
+    days: '18',
+    time1: 50
+  },
+  {
+    id: 8,
+    skill: 'Animation',
+    complete: 3.5,
+    zicopsCourse: 3,
+    days: '21',
+    time1: 60
+  }
+];
