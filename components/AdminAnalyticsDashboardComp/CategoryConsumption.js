455d248e8f81b77574e227d9d65eac02ffbef411
VAJRESH <vajresh005@gmail.com>
2023-02-16 01:50:32 +0530

    feat: sort added in cat consumption

--- components/AdminAnalyticsDashboardComp/CategoryConsumption.js
+++ components/AdminAnalyticsDashboardComp/CategoryConsumption.js
@@ -2,22 +2,40 @@ import BarChart from '@/components/common/Charts/BarChart';
 import Dropdown from '@/components/DashboardComponents/Dropdown';
 import { useHandleCatSubCat } from '@/helper/hooks.helper';
 import { useState } from 'react';
+import Spinner from '../common/Spinner';
 import styles from './adminAnalyticsDashboard.module.scss';
 import SectionTitle from './common/SectionTitle';
+import SortBtn from './common/SortBtn';
 import useHandleCatConsumption from './Logic/useHandleCatConsumption';
 
 export default function CategoryConsumption() {
   const [filters, setFilters] = useState({ category: null, subCategory: null });
+  const [sortIndex, setSortIndex] = useState({ x: 0, y: 0 });
   const { catSubCat, setActiveCatId } = useHandleCatSubCat();
-  const { subCatData } = useHandleCatConsumption();
+  const { subCatData, isLoading } = useHandleCatConsumption();
 
-  const dataArr = subCatData?.filter((subCat) => {
-    let isFiltered = true;
-    if (filters?.category) isFiltered = subCat?.cat?.Name === filters?.category;
-    if (filters?.subCategory) isFiltered = subCat?.Name === filters?.subCategory;
+  const sort = ['none', 'up', 'down'];
+  const dataArr = subCatData
+    ?.filter((subCat) => {
+      let isFiltered = true;
+      if (filters?.category) isFiltered = subCat?.cat?.Name === filters?.category;
+      if (filters?.subCategory) isFiltered = subCat?.Name === filters?.subCategory;
 
-    return isFiltered;
-  });
+      return isFiltered;
+    })
+    ?.sort((data1, data2) => {
+      const compare = data1?.name?.localeCompare(data2?.name);
+      if (sortIndex?.x === 1) return compare;
+      if (sortIndex?.x === 2) return compare * -1;
+
+      return -1;
+    })
+    ?.sort((data1, data2) => {
+      if (sortIndex?.y === 1) return data2?.count - data1?.count;
+      if (sortIndex?.y === 2) return data1?.count - data2?.count;
+
+      return -1;
+    });
   const categoryConsumptionData = {
     labels: dataArr?.map((data) => data.name),
     datasets: [
@@ -37,13 +55,13 @@ export default function CategoryConsumption() {
 
   function getDisplayHeading() {
     if (filters?.subCategory) return `${filters?.subCategory} courses`;
-    if (filters?.category) return `${filters?.category} categories courses`;
+    if (filters?.category) return `${filters?.category} sub-categories`;
 
     return 'All categories';
   }
 
   return (
-    <div className={`${styles.wrapper}`}>
+    <div className={`${styles.wrapper} ${styles.categoryConsumption}`}>
       <SectionTitle
         title="Category consumption"
         extraCompAtEnd={
@@ -71,8 +89,21 @@ export default function CategoryConsumption() {
 
       <div className={`${styles.wrapperSubHeading}`}>{getDisplayHeading()}</div>
 
-      <div>
-        {!!categoryConsumptionData?.labels?.length && (
+      <SortBtn
+        handleClick={() =>
+          setSortIndex((prev) => {
+            const index = prev.y + 1;
+            if (index === sort?.length) return { ...prev, y: 0 };
+            return { ...prev, y: index || 0 };
+          })
+        }
+        sortIndex={sortIndex.y}
+        isAxisX={false}
+        displayText="Number of courses"
+      />
+
+      <div className={`${styles.barGraphContainer}`}>
+        {!isLoading ? (
           <BarChart
             chartData={categoryConsumptionData}
             containerStyles={{
@@ -80,6 +111,24 @@ export default function CategoryConsumption() {
               height: '300px'
             }}
             labelLength={15}
+          />
+        ) : (
+          <Spinner />
+        )}
+      </div>
+
+      <div className={`${styles.footer}`}>
+        {!isLoading && (
+          <SortBtn
+            handleClick={() =>
+              setSortIndex((prev) => {
+                const index = prev.x + 1;
+                if (index === sort?.length) return { ...prev, x: 0 };
+                return { ...prev, x: index || 0 };
+              })
+            }
+            sortIndex={sortIndex.x}
+            displayText="Sub-Categories"
           />
         )}
       </div>
