98579810b0678b580d10b5dc0c3a5811195f09bb
shawnkit70 <ankitjha844@gmail.com>
2023-02-27 02:02:27 +0530

    course completed on issue fixed

--- components/common/CourseBoxCard/ProgressBarFooter.js
+++ components/common/CourseBoxCard/ProgressBarFooter.js
@@ -1,4 +1,3 @@
-import moment from 'moment';
 import styles from './courseBoxCard.module.scss';
 
 export default function ProgressBarFooter({ courseData, isCompleted }) {
@@ -9,7 +8,7 @@ export default function ProgressBarFooter({ courseData, isCompleted }) {
       <div>
         {isCompleted ? (
           <>
-            <p>Completed on {moment.unix(courseData?.updated_at).format("DD/MM/YYYY") || '22-06-2022'}</p>
+            <p>Completed on {courseData?.completedOn || '22-06-2022'}</p>
             <img src="/images/svg/green-tick.svg" alt="" height={15} />
           </>
         ) : (
