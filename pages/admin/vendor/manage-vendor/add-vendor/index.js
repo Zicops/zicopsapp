ce3c39f1063021721d89f29543a2fb54b11d2110
shawnkit70 <ankitjha844@gmail.com>
2023-02-26 22:09:13 +0530

    uncommented addUpdateVendor from vendor master

--- pages/admin/vendor/manage-vendor/add-vendor/index.js
+++ pages/admin/vendor/manage-vendor/add-vendor/index.js
@@ -54,7 +54,7 @@ export default function VendorInfo() {
             footerObj={{
               showFooter: true,
               handleSubmit: () => {
-                // addUpdateVendor();
+                addUpdateVendor();
                 handleMail();
                 // addUpdateSme();
               },
