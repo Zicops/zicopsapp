54c74451383e3eb1c3b63fdfb7126d4e0a58b35b
ANUPAM-RO <68887502+ANUPAM-RO@users.noreply.github.com>
2023-03-10 12:53:58 +0530

    profile vendor logic part move to useProfile hook

--- components/VendorComps/AddVendorProfile.js
+++ components/VendorComps/AddVendorProfile.js
@@ -1,5 +1,4 @@
 import LabeledInput from '@/components/common/FormComponents/LabeledInput';
-import React, { useState } from 'react';
 import styles from './vendorComps.module.scss';
 import LabeledTextarea from '@/components/common/FormComponents/LabeledTextarea';
 import BrowseAndUpload from '@/components/common/FormComponents/BrowseAndUpload';
@@ -8,89 +7,44 @@ import IconButton from '@/components/common/IconButton';
 import LabeledRadioCheckbox from '@/components/common/FormComponents/LabeledRadioCheckbox';
 import AddExpriences from './AddExpriences';
 import { VENDOR_LANGUAGES, VENDOR_MASTER_STATUS } from '@/helper/constants.helper';
-import { useRecoilState } from 'recoil';
-import { useRouter } from 'next/router';
-import {
-  getExperiencesObject,
-  VendorAllExperiencesAtom,
-  VendorExperiencesAtom,
-  VendorProfileAtom
-} from '@/state/atoms/vendor.atoms';
 import { changeHandler } from '@/helper/common.helper';
 import VendorPopUp from './common/VendorPopUp';
 import AddExpertise from './AddVendor/common/AddExpertise';
 import useHandleVendor from './Logic/useHandleVendor';
 import { optionYearArray } from './Logic/vendorComps.helper';
+import useProfile from './Logic/useProfile';
 
 const AddVendorProfile = ({ data = {} }) => {
-  const [isOpenExpriences, setIsOpenExpriences] = useState(false);
-  const [isOpenLanguage, setIsOpenLanguage] = useState(false);
-  const [isOpenSmeExpertise, setOpenSmeExpertise] = useState(false);
-  const [isOpenCrtExpertise, setOpenCrtExpertise] = useState(false);
-  const [isOpenCdExpertise, setOpenCdExpertise] = useState(false);
-  const [expertiseSearch, setExpertiseSearch] = useState('');
-  const [profileData, setProfileData] = useRecoilState(VendorProfileAtom);
-  const [experiencesData, setExperiencesData] = useRecoilState(VendorExperiencesAtom);
-  const [selectedSmeExpertise, setSelectedSmeExpertise] = useState([]);
-  const [selectedCrtExpertise, setSelectedCrtExpertise] = useState([]);
-  const [selectedCdExpertise, setSelectedCdExpertise] = useState([]);
-  const [selectedLanguages, setSelectedLanguages] = useState([]);
-
   const { handleProfilePhoto, getSingleExperience } = useHandleVendor();
-  const router = useRouter();
-  const vendorId = router.query.vendorId || '0';
-  console.info('experiencesData', profileData?.experience);
-
-  const completeExperienceHandler = () => {
-    const StartDate = experiencesData?.startMonth?.concat('-', experiencesData?.startYear);
-    const start_date = new Date(StartDate);
-    const start_timestamp = start_date.getTime() / 1000;
-    const EndDate = experiencesData?.endMonth?.concat('-', experiencesData?.endYear);
-    const end_date = new Date(EndDate);
-    const end_timestamp = end_date.getTime() / 1000;
-    const experienceData = {
-      vendor_id: vendorId || '',
-      title: experiencesData?.title || '',
-      email: profileData?.email || '',
-      company_name: experiencesData?.companyName || '',
-      employement_type: experiencesData?.employeeType || '',
-      location: experiencesData?.location || '',
-      location_type: experiencesData?.locationType || '',
-      start_date: start_timestamp || null,
-      end_date: end_timestamp || null,
-      status: VENDOR_MASTER_STATUS.active
-    };
-    setProfileData((prev) => ({ ...prev, experience: [...prev?.experience, experienceData] }));
-    setIsOpenExpriences(false);
-    setExperiencesData(getExperiencesObject());
-  };
-
-  const handleLanguageSelection = (e) => {
-    const { value, checked } = e.target;
-    if (checked) {
-      setSelectedLanguages([...selectedLanguages, value]);
-    } else {
-      setSelectedLanguages(selectedLanguages?.filter((lang) => lang !== value));
-    }
-  };
-
-  const addLanguagesHandler = () => {
-    setProfileData({ ...profileData, languages: [...selectedLanguages] });
-    setIsOpenLanguage(false);
-  };
-
-  const handleAddSmeExpertise = () => {
-    setProfileData({ ...profileData, sme_expertises: [...selectedSmeExpertise] });
-    setOpenSmeExpertise(false);
-  };
-  const handleAddCrtExpertise = () => {
-    setProfileData({ ...profileData, crt_expertises: [...selectedCrtExpertise] });
-    setOpenCrtExpertise(false);
-  };
-  const handleAddCdExpertise = () => {
-    setProfileData({ ...profileData, content_development: [...selectedCdExpertise] });
-    setOpenCdExpertise(false);
-  };
+  const {
+    completeExperienceHandler,
+    handleLanguageSelection,
+    addLanguagesHandler,
+    handleAddSmeExpertise,
+    handleAddCrtExpertise,
+    handleAddCdExpertise,
+    isOpenExpriences,
+    setIsOpenExpriences,
+    isOpenLanguage,
+    setIsOpenLanguage,
+    isOpenSmeExpertise,
+    setOpenSmeExpertise,
+    isOpenCrtExpertise,
+    setOpenCrtExpertise,
+    isOpenCdExpertise,
+    setOpenCdExpertise,
+    expertiseSearch,
+    setExpertiseSearch,
+    profileData,
+    setProfileData,
+    selectedSmeExpertise,
+    setSelectedSmeExpertise,
+    selectedCrtExpertise,
+    setSelectedCrtExpertise,
+    selectedCdExpertise,
+    setSelectedCdExpertise,
+    selectedLanguages
+  } = useProfile();
 
   return (
     <div className={`${styles.inputMain}`}>
