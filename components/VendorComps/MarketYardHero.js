3f3e88599ff6e8db283e630a139559e98d08fe16
ANUPAM-RO <68887502+ANUPAM-RO@users.noreply.github.com>
2023-03-14 19:44:47 +0530

    market yard profile data added and order api added

--- components/VendorComps/MarketYardHero.js
+++ components/VendorComps/MarketYardHero.js
@@ -1,13 +1,17 @@
-import styles from './vendorComps.module.scss';
+import { VendorStateAtom } from '@/state/atoms/vendor.atoms';
+import { useRecoilValue } from 'recoil';
 import { MarketYardHeroData } from './Logic/vendorComps.helper';
+import styles from './vendorComps.module.scss';
 const MarketYardHero = ({ onHandlePopup }) => {
+  const vendorData = useRecoilValue(VendorStateAtom);
+
   return (
     <div className={`${styles.marketHeroContainer}`}>
       <div className={`${styles.marketHeroDetails}`}>
         <div className={`${styles.courseImage}`}>
-          <img src="/images/discord_logo.png" alt="" />
+          <img src={vendorData?.vendorProfileImage || '/images/discord_logo.png'} alt="" />
         </div>
-        <p className={`${styles.companyName}`}>{MarketYardHeroData?.companyName}</p>
+        <p className={`${styles.companyName}`}>{vendorData?.name}</p>
         <div className={`${styles.expartContainer}`}>
           {MarketYardHeroData?.expart?.map((data, index) => (
             <div className={`${styles.expart}`} key={index}>
