58c83e5e7e1c9ea807a812996d531f1662d29953
VAJRESH <vajresh005@gmail.com>
2023-03-16 03:06:41 +0530

    feat: courses tab set

--- components/VendorComps/AddVendor/AddVendorCourses.js
+++ components/VendorComps/AddVendor/AddVendorCourses.js
@@ -1,18 +1,40 @@
 import ZicopsCarousel from '@/components/ZicopsCarousel';
-import { useEffect } from 'react';
-import useHandleVendor from '../Logic/useHandleVendor';
+import { COURSE_STATUS } from '@/helper/constants.helper';
+import { useEffect, useState } from 'react';
+import useHandleVendorCourses from '../Logic/useHandleVendorCourses';
+import styles from './addvendor.module.scss';
+
+const skeletonCardCount = 5;
 
 export default function AddVendorCourses() {
-  const { getVendorCourses, vendorCourses } = useHandleVendor();
-  console.info('vendorCourses', vendorCourses);
+  const [savedCourses, setSavedCourses] = useState([...Array(skeletonCardCount)]);
+  const [forApprovalCourses, setForApprovalCourses] = useState([...Array(skeletonCardCount)]);
+  const [publishedCourses, setPublishedCourses] = useState([...Array(skeletonCardCount)]);
+
+  const { getVendorCourses } = useHandleVendorCourses();
 
   useEffect(() => {
-    getVendorCourses();
+    getVendorCourses(COURSE_STATUS.save).then((courses) => setSavedCourses(courses));
+    getVendorCourses(COURSE_STATUS.approvalPending).then((courses) =>
+      setForApprovalCourses(courses)
+    );
+    getVendorCourses(COURSE_STATUS.publish).then((courses) => setPublishedCourses(courses));
   }, []);
 
   return (
     <div>
-      <ZicopsCarousel title="Draft Courses" type="small" data={vendorCourses} />
+      {!savedCourses?.length && !forApprovalCourses?.length && !publishedCourses?.length && (
+        <div className={styles.fallback}>No Courses Added</div>
+      )}
+      {!!savedCourses?.length && (
+        <ZicopsCarousel title="Saved Courses" type="small" data={savedCourses} />
+      )}
+      {!!forApprovalCourses?.length && (
+        <ZicopsCarousel title="Send For Approval Courses" type="small" data={forApprovalCourses} />
+      )}
+      {!!publishedCourses?.length && (
+        <ZicopsCarousel title="Published Courses" type="small" data={publishedCourses} />
+      )}
     </div>
   );
 }
