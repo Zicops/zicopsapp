37309408a8b5ace6a065192f04bb5800c91c2fd7
VAJRESH <vajresh005@gmail.com>
2023-03-21 18:01:53 +0530

    feat: manage vendor course tab add

--- components/VendorComps/AddVendor/AddVendorCourses.js
+++ components/VendorComps/AddVendor/AddVendorCourses.js
@@ -1,16 +1,16 @@
-import ZicopsCarousel from '@/components/ZicopsCarousel';
+import CardContainer from '@/components/LearnerUserProfile/UserCoursesTab/CardContainer';
 import { COURSE_STATUS } from '@/helper/constants.helper';
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import useHandleVendorCourses from '../Logic/useHandleVendorCourses';
 import styles from './addvendor.module.scss';
 
-const skeletonCardCount = 5;
-
 export default function AddVendorCourses() {
-  const [savedCourses, setSavedCourses] = useState([...Array(skeletonCardCount)]);
-  const [forApprovalCourses, setForApprovalCourses] = useState([...Array(skeletonCardCount)]);
-  const [publishedCourses, setPublishedCourses] = useState([...Array(skeletonCardCount)]);
+  const [savedCourses, setSavedCourses] = useState([]);
+  const [forApprovalCourses, setForApprovalCourses] = useState([]);
+  const [publishedCourses, setPublishedCourses] = useState([]);
 
+  const router = useRouter();
   const { getVendorCourses } = useHandleVendorCourses();
 
   useEffect(() => {
@@ -21,20 +21,51 @@ export default function AddVendorCourses() {
     getVendorCourses(COURSE_STATUS.publish).then((courses) => setPublishedCourses(courses));
   }, []);
 
+  const courseSections = [
+    {
+      displayType: 'Saved Courses',
+      footerType: 'adminFooter',
+      data: savedCourses,
+      buttonText: 'Edit',
+      handleClick: (course) => router.push(`/admin/courses/${course?.id}`)
+    },
+    {
+      displayType: 'Send For Approval Courses',
+      footerType: 'adminFooter',
+      buttonText: 'View',
+      data: forApprovalCourses,
+      handleClick: (course) => router.push(`/admin/courses/${course?.id}`)
+    },
+    {
+      displayType: 'Published Courses',
+      footerType: 'adminFooter',
+      buttonText: 'View',
+      data: publishedCourses,
+      handleClick: (course) => router.push(`/admin/courses/${course?.id}`)
+    }
+  ];
+
   return (
-    <div>
+    <div className={styles.vendorCourseTab}>
       {!savedCourses?.length && !forApprovalCourses?.length && !publishedCourses?.length && (
         <div className={styles.fallback}>No Courses Added</div>
       )}
-      {!!savedCourses?.length && (
-        <ZicopsCarousel title="Saved Courses" type="small" data={savedCourses} />
-      )}
-      {!!forApprovalCourses?.length && (
-        <ZicopsCarousel title="Send For Approval Courses" type="small" data={forApprovalCourses} />
-      )}
-      {!!publishedCourses?.length && (
-        <ZicopsCarousel title="Published Courses" type="small" data={publishedCourses} />
-      )}
+      {courseSections.map((section, index) => {
+        return (
+          <div key={index}>
+            <CardContainer
+              type={section?.displayType}
+              footerType={section?.footerType}
+              courseData={section?.data}
+              isRemove={section?.isRemove ? section?.isRemove : false}
+              handleSubmit={section?.handleClick ? section?.handleClick : () => {}}
+              buttonText={section?.buttonText ? section?.buttonText : ''}
+              isAdmin={true}
+              customStyles={{ fontSize: '15px' }}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
