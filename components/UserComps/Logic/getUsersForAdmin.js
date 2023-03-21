aeb7708ee130038e1de878a4a4271704d39b135e
shawnkit70 <ankitjha844@gmail.com>
2023-03-22 00:51:43 +0530

    feat: added tags in userTable

--- components/UserComps/Logic/getUsersForAdmin.js
+++ components/UserComps/Logic/getUsersForAdmin.js
@@ -1,3 +1,4 @@
+import { GET_USER_LSP_TAGS, notificationClient } from '@/api/NotificationClient';
 import {
   GET_LSP_USERS_WITH_ROLE,
   GET_USER_DETAIL,
@@ -5,9 +6,10 @@ import {
   userQueryClient
 } from '@/api/UserQueries';
 import { loadQueryDataAsync } from '@/helper/api.helper';
-import { USER_LSP_ROLE } from '@/helper/constants.helper';
+import { USER_LSP_ROLE, USER_TYPE } from '@/helper/constants.helper';
+import { FcmTokenAtom } from '@/state/atoms/notification.atom';
 import { UsersOrganizationAtom } from '@/state/atoms/users.atom';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 export async function getUsersForAdmin(isAdmin = false) {
   const lspId = sessionStorage.getItem('lsp_id');
@@ -75,7 +77,17 @@ export async function getUsersForAdmin(isAdmin = false) {
 
 export default function useAdminQuery() {
   const [userOrgData, setUserOrgData] = useRecoilState(UsersOrganizationAtom);
+  const fcmToken = useRecoilValue(FcmTokenAtom);
 
+  async function getTag(tagArr = []){
+    if(!tagArr?.length) return USER_TYPE?.external;
+    let tag = USER_TYPE?.external ;
+    for(let i = 0 ; i < tagArr?.length ; i++){
+      const currentIndexTag = tagArr[i]?.toLowerCase();
+      if(currentIndexTag === USER_TYPE?.external || currentIndexTag === USER_TYPE?.internal ) tag = currentIndexTag ;
+    }
+    return tag
+  }
   async function sortArray(arr, param) {
     const sortedArr = await arr?.sort((a, b) => a?.[`${param}`] - b?.[`${param}`]);
     return sortedArr;
@@ -90,9 +102,23 @@ export default function useAdminQuery() {
       userQueryClient
     );
 
-    if (usersResult?.error) return { error: 'Error occured while while loading lsp Users!' };
-    const formatedUsers = usersResult?.getPaginatedLspUsersWithRoles?.data?.map(
-      async (singleUser) => {
+    if (usersResult?.error) return { error: 'Error occured while loading lsp Users!' };
+    const userDatas = usersResult?.getPaginatedLspUsersWithRoles?.data?.filter((user) => !!user);
+    const userLspIds = userDatas?.map((userData) => {
+      return userData?.roles?.[0]?.user_lsp_id;
+    });
+
+    const tags = await loadQueryDataAsync(
+      GET_USER_LSP_TAGS,
+      { user_lsp_ids: userLspIds },
+      { context: { headers: { 'fcm-token': fcmToken || sessionStorage?.getItem('fcm-token') } } },
+      notificationClient
+    );
+
+    const userTags = tags?.getUserLspIdTags ;
+
+    const formatedUsers =
+      userDatas?.map(async (singleUser,index) => {
         let roles = singleUser?.roles;
         let roleData = {};
 
@@ -102,6 +128,9 @@ export default function useAdminQuery() {
         } else {
           roleData = roles?.[0];
         }
+        const vendorTag = roleData?.role?.toLowerCase() === USER_LSP_ROLE?.vendor ? USER_TYPE?.external : USER_TYPE?.internal
+        const type = !!userTags?.[index] ? await getTag(userTags?.[index]?.tags) : vendorTag ;
+        
         return {
           ...singleUser?.user,
           role: roleData?.role,
@@ -113,10 +142,9 @@ export default function useAdminQuery() {
           full_name: `${singleUser?.user?.first_name} ${singleUser?.user?.last_name}`,
           user_lsp_id: roleData?.user_lsp_id,
           lsp_status: singleUser?.status,
-          type: roleData?.role?.toLowerCase() === USER_LSP_ROLE?.vendor ? 'external' : 'internal'
+          type: type
         };
-      }
-    ) || [];
+      }) || [];
 
     const users = Promise?.all(formatedUsers);
 
