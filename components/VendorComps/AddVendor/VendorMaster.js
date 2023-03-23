6702f5ae0b0746f1a2a62a029ae670085738d86c
Jayanta Sarkar <jtas.sarkar@gmail.com>
2023-03-23 21:12:07 +0530

    vendor description length changed to 500

--- components/VendorComps/AddVendor/VendorMaster.js
+++ components/VendorComps/AddVendor/VendorMaster.js
@@ -164,7 +164,7 @@ export default function VendorMaster() {
           inputOptions={{
             inputName: 'description',
             placeholder: 'Say Something...',
-            maxLength: 160,
+            maxLength: 500,
             value: vendorData?.description,
             isDisabled: isViewPage
           }}
