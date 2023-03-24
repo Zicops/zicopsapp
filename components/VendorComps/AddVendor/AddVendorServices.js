f74737251595085096d26b231f9f2741e2112847
VAJRESH <vajresh005@gmail.com>
2023-03-24 13:56:52 +0530

    feat: dev flag added in vendor services

--- components/VendorComps/AddVendor/AddVendorServices.js
+++ components/VendorComps/AddVendor/AddVendorServices.js
@@ -1,7 +1,7 @@
 import ZicopsAccordian from '@/common/ZicopsAccordian';
+import { FeatureFlagsAtom } from '@/state/atoms/global.atom';
 import { CdServicesAtom, CtServicesAtom, SmeServicesAtom } from '@/state/atoms/vendor.atoms';
-import { useEffect } from 'react';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import styles from '../vendorComps.module.scss';
 import AddServices from './common/AddServices';
 
@@ -9,6 +9,8 @@ export default function AddVendorServices() {
   const [smeData, setSMEData] = useRecoilState(SmeServicesAtom);
   const [ctData, setCTData] = useRecoilState(CtServicesAtom);
   const [cdData, setCDData] = useRecoilState(CdServicesAtom);
+  const { isDev } = useRecoilValue(FeatureFlagsAtom);
+
   const ptype = [{ SME: 'sme', CRT: 'crt', CD: 'cd' }];
   const servicesHelper = [
     {
@@ -17,7 +19,8 @@ export default function AddVendorServices() {
       title: 'Subject Matter Expertise',
       inputName: 'isApplicable',
       experticeName: 'Add Subject Matter Expertise',
-      ptype: ptype[0]?.SME
+      ptype: ptype[0]?.SME,
+      isDev: true
     },
     {
       data: ctData,
@@ -33,13 +36,16 @@ export default function AddVendorServices() {
       title: 'Content Development',
       inputName: 'isApplicable',
       experticeName: 'Add Content Development Expertise',
-      ptype: ptype[0]?.CD
+      ptype: ptype[0]?.CD,
+      isDev: true
     }
   ];
 
   return (
     <div className={`${styles.addServiceContainer}`}>
       {servicesHelper.map((value, index) => {
+        if (value?.isDev && !isDev) return;
+
         return (
           <ZicopsAccordian title={value.title}>
             <AddServices
