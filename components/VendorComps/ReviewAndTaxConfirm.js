8cc36b0047de356b1e95df684a3231ec84f00957
ANUPAM-RO <68887502+ANUPAM-RO@users.noreply.github.com>
2023-03-30 12:47:45 +0530

    confirm page checkbox logic added

--- components/VendorComps/ReviewAndTaxConfirm.js
+++ components/VendorComps/ReviewAndTaxConfirm.js
@@ -8,11 +8,34 @@ const ReviewAndTaxConfirm = () => {
   const [orderData, setOrderData] = useRecoilState(OrderAtom);
   const [servicesData, setServicesData] = useRecoilState(ServicesAtom);
 
-  const taxAmount = (orderData?.total * 10) / 100;
+  const orderArray = [];
+  if (servicesData?.sme?.length) {
+    orderArray.push(...servicesData?.sme);
+  }
+  if (servicesData?.crt?.length) {
+    orderArray.push(...servicesData?.crt);
+  }
+  if (servicesData?.cd?.length) {
+    orderArray.push(...servicesData?.cd);
+  }
+  if (servicesData?.speakers?.length) {
+    orderArray.push(...servicesData?.speakers);
+  }
+
+  const subtotalArray = orderArray?.map((data) => {
+    if (!data?.isActive) return null;
+    return data?.total;
+  });
+  const subtotal = subtotalArray?.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+
+  const taxAmount = (subtotal * 10) / 100;
+
   const grossTotal = orderData?.total + taxAmount;
 
   useEffect(() => {
-    setOrderData({ ...orderData, tax: taxAmount, grossTotal: grossTotal });
+    setOrderData({ ...orderData, total: subtotal, tax: taxAmount, grossTotal: grossTotal });
   }, [servicesData]);
   return (
     <div>
