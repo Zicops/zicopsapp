6599556aacb3e008805169bd2086a741678458d5
sandeepkundekar <94161784+sandeepkundekar@users.noreply.github.com>
2023-03-31 15:03:11 +0530

    feat :updated vilt admin card

--- components/Vctools/SessionJoinCard/index.js
+++ components/Vctools/SessionJoinCard/index.js
@@ -2,26 +2,29 @@ import { useEffect, useState } from 'react';
 import styles from './sessionJoin.module.scss';
 const SessionJoinCard = () => {
   const [timing, setTiming] = useState({
-    day: 3,
-    month: 3,
-    year: 2029,
-    time: 23,
-    timeMin: 30,
+    // meeting start time
+    day: new Date().getDate(),
+    month: new Date().getMonth(),
+    year: new Date().getFullYear(),
+    time: 12,
+    timeMin: 29
     // timeSec: 0
   });
-  //   {new Date(`${dateInfo.month}/${dateInfo.day}/${dateInfo.year} ${dateInfo.hours}:
-  //         ${dateInfo.minute}:${dateInfo.second}`).getTime()}
-  let givenTime =
-    new Date(
-      `${timing.month}/ ${timing.day}/${timing.year}  ${timing.time}:${timing.timeMin}:${timing.timeSec}`
-    ).getTime() / 1000;
   let duration = 1800;
+  const [meetingStart, setmeetingStart] = useState(new Date('3/31/2023 14:58:00').getTime());
+  const [meetingEnd, setmeetingEnd] = useState(new Date('3/31/2023 15:30:00').getTime());
+
   const [meetingDuration, setMeetingDuration] = useState(30);
   const [meetingType, setMeetingType] = useState('');
   useEffect(() => {
-    if (givenTime <= new Date().getTime()) {
+    var now = new Date().getTime();
+    if (now >= meetingStart && now <= meetingEnd) {
+      setMeetingType('MeetingStarted');
+    } else if (now < meetingStart) {
       setMeetingType('JoinSession');
-    } 
+    } else {
+      setMeetingType('MeetingEnded');
+    }
   });
   const title = ['Session has not started', 'Live now', 'Session Ended'];
   return (
@@ -29,24 +32,30 @@ const SessionJoinCard = () => {
       <div>
         <div className={`${styles.joinSessionHead}`}>
           {/* <div>Session has not started</div> */}
-          {meetingType === 'JoinSession' ? (
-            <div>{title[0]}</div>
-          ) : meetingType === 'MeetingStarted' ? (
-            <div>{title[1]}</div>
-          ) : (
-            <div>{title[2]}</div>
+          {meetingType === 'JoinSession' && <div>{title[0]}</div>}
+          {meetingType === 'MeetingStarted' && (
+            <div className={`${styles.meetingLive}`}>
+              {title[1]} <img src="/images/svg/vctool/sensors-on.svg" />
+            </div>
+          )}
+          {meetingType === 'MeetingEnded' && (
+            <div className={`${styles.sessionEnd}`}>
+              {title[2]} <img src="/images/svg/vctool/sensors-off.svg" />
+            </div>
           )}
         </div>
         {/* <button className={`${styles.JoinSessionBtn}`}>Join Session</button> */}
-        {meetingType === 'JoinSession' ? (
+        {meetingType === 'JoinSession' && (
           <button className={`${styles.JoinSessionBtn}`}>Join Session</button>
-        ) : meetingType === 'MeetingStarted' ? (
+        )}
+        {meetingType === 'MeetingStarted' && (
           <button className={`${styles.JoinSessionBtn}`}>Join Session</button>
-        ) : (
+        )}
+        {meetingType === 'MeetingEnded' && (
           <button className={`${styles.sessionEnded}`}>Recording will be available soon</button>
         )}
         <div className={`${styles.joinsessionFooter}`}>
-          <div>{`0${timing.day}-0${timing.month}-${timing.year},
+          <div>{`${timing.day}-${timing.month}-${timing.year},
           ${timing.time}:${timing.timeMin} IST`}</div>
           <div className={`${styles.meetingDuration}`}>
             duration :<div className={`${styles.durationTime}`}>{meetingDuration} min</div>
