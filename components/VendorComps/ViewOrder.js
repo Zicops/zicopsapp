7d7520d60b32161299dde4d800afaab200b8c903
ANUPAM-RO <68887502+ANUPAM-RO@users.noreply.github.com>
2023-04-13 12:05:03 +0530

    confirm and reject confirmation popup added

--- components/VendorComps/ViewOrder.js
+++ components/VendorComps/ViewOrder.js
@@ -15,14 +15,17 @@ import { useRecoilState } from 'recoil';
 import useHandleVendor from './Logic/useHandleVendor';
 import { VENDOR_ORDER_STATUS, VENDOR_SERVICES_TYPE } from '@/helper/constants.helper';
 import Loader from '../common/Loader';
+import ConfirmPopUp from '../common/ConfirmPopUp';
 
-const ViewOrder = ({ orderId = null, viewOrder, setViewOrder }) => {
+const ViewOrder = ({ orderId = null, viewOrder, setViewOrder, onSuccess = () => {} }) => {
   const [currentComponent, setCurrentComponent] = useState(0);
   const [selectedServicesForOrder, setSelectedServicesForOrder] =
     useRecoilState(VendorServicesListAtom);
   const [allServicesData, setAllServicesData] = useRecoilState(AllServicesAtom);
   const [servicesData, setServicesData] = useRecoilState(ServicesAtom);
   const [orderData, setOrderData] = useRecoilState(OrderAtom);
+  const [isRejectOrder, setRejectOrder] = useState(false);
+  const [isConfirmOrder, setConfirmOrder] = useState(false);
   const { getSingleVendorInfo } = useHandleVendor();
   const {
     addUpdateOrder,
@@ -73,79 +76,128 @@ const ViewOrder = ({ orderId = null, viewOrder, setViewOrder }) => {
 
   const confirmOrderHandler = async () => {
     setCurrentComponent(currentComponent + 1);
+    const _orderData = structuredClone(orderData);
     if (currentComponent === 0) {
-      setOrderData({ ...orderData, status: VENDOR_ORDER_STATUS?.confirmed });
-      await addUpdateOrder(orderInfo[0]?.vendor_id, orderInfo[0]?.id);
+      _orderData.status = VENDOR_ORDER_STATUS?.confirmed;
+      await addUpdateOrder(
+        orderInfo[0]?.vendor_id,
+        orderInfo[0]?.id,
+        VENDOR_ORDER_STATUS?.confirmed
+      );
+      setConfirmOrder(false);
     }
     if (currentComponent === 1) {
-      setOrderData({ ...orderData, status: VENDOR_ORDER_STATUS?.completed });
-      await addUpdateOrder(orderInfo[0]?.vendor_id, orderInfo[0]?.id);
+      _orderData.status = VENDOR_ORDER_STATUS?.completed;
+      await addUpdateOrder(
+        orderInfo[0]?.vendor_id,
+        orderInfo[0]?.id,
+        VENDOR_ORDER_STATUS?.completed
+      );
     }
     if (currentComponent === 2) {
       setViewOrder(false);
       setCurrentComponent(0);
-      setServicesData(getVendorServicesObject());
-      setSelectedServicesForOrder(getVendorServicesList());
-      setOrderData(getVendorOrderObject());
-      setAllServicesData([]);
+      // setServicesData(getVendorServicesObject());
+      // setSelectedServicesForOrder(getVendorServicesList());
+      // setOrderData(getVendorOrderObject());
+      // setAllServicesData([]);
     }
+
+    onSuccess(_orderData.status);
+    setOrderData(_orderData);
   };
   const rejectOrderHandler = async () => {
-    setOrderData({ ...orderData, status: VENDOR_ORDER_STATUS?.rejected });
-    await addUpdateOrder(orderInfo[0]?.vendor_id, orderInfo[0]?.id);
+    const _orderData = structuredClone(orderData);
+    _orderData.status = VENDOR_ORDER_STATUS?.rejected;
+    await addUpdateOrder(orderInfo[0]?.vendor_id, orderInfo[0]?.id, VENDOR_ORDER_STATUS?.rejected);
+    onSuccess(_orderData.status);
+    setOrderData(_orderData);
     setViewOrder(false);
-    setServicesData(getVendorServicesObject());
-    setSelectedServicesForOrder(getVendorServicesList());
-    setOrderData(getVendorOrderObject());
-    setAllServicesData([]);
+    setRejectOrder(false);
+    // setServicesData(getVendorServicesObject());
+    // setSelectedServicesForOrder(getVendorServicesList());
+    // setOrderData(getVendorOrderObject());
+    // setAllServicesData([]);
   };
 
   return (
-    <VendorPopUp
-      open={viewOrder}
-      popUpState={[viewOrder, setViewOrder]}
-      title="View Order"
-      size="large"
-      closeBtn={{ name: 'Reject', handleClick: rejectOrderHandler }}
-      isCloseButton={currentComponent === 0}
-      onCloseWithCross={() => {
-        setCurrentComponent(0);
-        setAllServicesData([]);
-      }}
-      submitBtn={{
-        name: currentComponent === 0 ? 'Confirm' : 'OK',
-        handleClick: confirmOrderHandler
-      }}>
-      <div>
-        {!allServicesData?.length ? (
-          <Loader customStyles={{ height: '100%', background: 'transparent' }} />
-        ) : (
-          <>
-            {currentComponent === 0 && (
-              <ReviewAndTaxConfirm
-                isViewOrder={true}
-                orderStatus="Added"
-                currency={allServicesData[0]?.currency}
-              />
-            )}
-            {currentComponent === 1 && (
-              <ReviewAndTaxConfirm
-                isViewOrder={true}
-                orderStatus="Confirmed"
-                currency={allServicesData[0]?.currency}
-              />
-            )}
-            {currentComponent === 2 && (
-              <ReviewAndTaxConfirm
-                isViewOrder={true}
-                orderStatus="Completed"
-                currency={allServicesData[0]?.currency}
-              />
-            )}
-          </>
-        )}
-      </div>
-    </VendorPopUp>
+    <>
+      <VendorPopUp
+        open={viewOrder}
+        popUpState={[viewOrder, setViewOrder]}
+        title="View Order"
+        size="large"
+        closeBtn={{ name: 'Reject', handleClick: () => setRejectOrder(true) }}
+        isCloseButton={currentComponent === 0}
+        onCloseWithCross={() => {
+          setCurrentComponent(0);
+          // setAllServicesData([]);
+        }}
+        submitBtn={{
+          name: currentComponent === 0 ? 'Confirm' : 'OK',
+          handleClick: () => {
+            if (currentComponent === 0) {
+              setConfirmOrder(true);
+            } else {
+              confirmOrderHandler();
+            }
+          }
+        }}>
+        <div>
+          {!allServicesData?.length ? (
+            <Loader customStyles={{ height: '100%', background: 'transparent' }} />
+          ) : (
+            <>
+              {currentComponent === 0 && (
+                <ReviewAndTaxConfirm
+                  isViewOrder={true}
+                  orderStatus="Added"
+                  currency={allServicesData[0]?.currency}
+                />
+              )}
+              {currentComponent === 1 && (
+                <ReviewAndTaxConfirm
+                  isViewOrder={true}
+                  orderStatus="Confirmed"
+                  currency={allServicesData[0]?.currency}
+                />
+              )}
+              {currentComponent === 2 && (
+                <ReviewAndTaxConfirm
+                  isViewOrder={true}
+                  orderStatus="Completed"
+                  currency={allServicesData[0]?.currency}
+                />
+              )}
+            </>
+          )}
+        </div>
+      </VendorPopUp>
+      {!!isRejectOrder && (
+        <ConfirmPopUp
+          title={`Are you sure to reject this order?`}
+          btnObj={{
+            handleClickLeft: (e) => {
+              e.currentTarget.disabled = true;
+              rejectOrderHandler();
+            },
+            handleClickRight: () => setRejectOrder(false)
+          }}
+        />
+      )}
+      {!!isConfirmOrder && (
+        <ConfirmPopUp
+          title={`Are you sure to Confirm this order?`}
+          btnObj={{
+            handleClickLeft: (e) => {
+              e.currentTarget.disabled = true;
+              confirmOrderHandler();
+            },
+            handleClickRight: () => setConfirmOrder(false)
+          }}
+        />
+      )}
+    </>
   );
 };
 export default ViewOrder;
